c122bdc45cb4bb1a555fa126ab036c10e7d31534
VDanchev <viktordan315@gmail.com>
2022-10-17 16:35:38 +0300

    Tasks Added

---
+++ CSharp-Fundamentals/BasicSyntaxConditionalStatementsAndLoopsLab/06.ForeignLanguages/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06.ForeignLanguages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string countryName = Console.ReadLine();
+
+            if (countryName == "England" || countryName == "USA")
+            {
+                Console.WriteLine("English");
+            }
+            else if (countryName == "Spain" || countryName == "Argentina" || countryName == "Mexico")
+            {
+                Console.WriteLine("Spanish");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
