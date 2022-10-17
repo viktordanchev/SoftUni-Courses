9757b577d91910d2442e14dd2b0d36dd1a41b5c3
VDanchev <viktordan315@gmail.com>
2022-10-17 16:40:10 +0300

    Tasks Added

---
+++ CSharp-Fundamentals/BasicSyntaxConditionalStatementsAndLoopsLab/09.SumОfOddNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _09.SumОfOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= number * 2; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
