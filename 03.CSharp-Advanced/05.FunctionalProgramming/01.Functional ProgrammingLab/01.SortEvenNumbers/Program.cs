e3d1371a5ce6cc46aa35f363cc64847a85c9ca12
VDanchev <viktordan315@gmail.com>
2023-01-24 17:58:07 +0200

    Solution and projects added

---
+++ 03.CSharp-Advanced/05.FunctionalProgramming/01.Functional ProgrammingLab/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.SortEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
