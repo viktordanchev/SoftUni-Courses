053ce591b789789c8b7adb15a836e6d153e58f6f
VDanchev <viktordan315@gmail.com>
2023-01-26 10:49:26 +0200

    Tasks Added

---
+++ 03.CSharp-Advanced/05.FunctionalProgramming/02.FunctionalProgrammingExercise/01.ActionPrint/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01.ActionPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+
+            Action<string> printName = n => Console.WriteLine(n);
+
+            foreach (var name in names)
+            {
+                printName(name);
+            }
+        }
+    }
+}
