25a5460b0595be31ea3a5415272d145bf4c13279
VDanchev <viktordan315@gmail.com>
2023-02-04 20:22:03 +0200

    Tasks Added

---
+++ 03.CSharp-Advanced/07.Generics/01.GenericsLab/01.Box/Box.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private int count;
+        private List<T> list;
+
+        public Box()
+        {
+            list = new List<T>();
+        }
+
+        public int Count { get { return count; } }
+
+        public void Add(T element)
+        {
+            count++;
+            list.Add(element);
+        }
+
+        public T Remove()
+        {
+            count--;
+            T elementToRemove = list[list.Count - 1];
+            list.Remove(elementToRemove);
+
+            return elementToRemove;
+        }
+    }
+}
