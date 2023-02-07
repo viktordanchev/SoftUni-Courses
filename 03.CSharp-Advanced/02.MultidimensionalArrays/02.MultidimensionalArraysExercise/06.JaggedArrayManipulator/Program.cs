01489d046387d9ff10006d59460741d01e2ee078
VDanchev <viktordan315@gmail.com>
2023-02-07 14:32:09 +0200

    Task Added

--- 03.CSharp-Advanced/02.MultidimensionalArrays/02.MultidimensionalArraysExercise/06.JaggedArrayManipulator/Program.cs
+++ 03.CSharp-Advanced/02.MultidimensionalArrays/02.MultidimensionalArraysExercise/06.JaggedArrayManipulator/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Threading;
 
 namespace _06.JaggedArrayManipulator
 {
@@ -9,6 +8,7 @@ namespace _06.JaggedArrayManipulator
         static void Main(string[] args)
         {
             int rows = int.Parse(Console.ReadLine());
+
             int[][] jaggedArray = new int[rows][];
 
             for (int row = 0; row < rows; row++)
@@ -17,13 +17,50 @@ namespace _06.JaggedArrayManipulator
             }
 
             CheckArrayForEqualRowsLength(jaggedArray);
+
+            string[] data = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            while (data[0] != "End")
+            {
+                string command = data[0];
+                int row = int.Parse(data[1]);
+                int col = int.Parse(data[2]);
+                int value = int.Parse(data[3]);
+
+                if (row < 0 || row >= jaggedArray.Length || col < 0 || jaggedArray[row].Length - 1 < col)
+                {
+                    data = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    continue;
+                }
+
+                switch (command)
+                {
+                    case "Add":
+                        jaggedArray[row][col] += value;
+                        break;
+                    case "Subtract":
+                        jaggedArray[row][col] -= value;
+                        break;
+                }
+
+                data = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            Print(jaggedArray);
+        }
+
+        static void Print(int[][] jaggedArray)
+        {
+            for (int row = 0; row < jaggedArray.Length; row++)
+            {
+                Console.WriteLine(string.Join(" ", jaggedArray[row]));
+            }
         }
 
         static void CheckArrayForEqualRowsLength(int[][] jaggedArray)
         {
-            for (int row = 0; row < jaggedArray.Length; row++)
+            for (int row = 0; row < jaggedArray.Length - 1; row++)
             {
-                for (int col = 0; col < jaggedArray[row].Length; col++)
+                for (int col = 0; col < Math.Max(jaggedArray[row].Length, jaggedArray[row + 1].Length); col++)
                 {
                     if (jaggedArray[row].Length == jaggedArray[row + 1].Length)
                     {
