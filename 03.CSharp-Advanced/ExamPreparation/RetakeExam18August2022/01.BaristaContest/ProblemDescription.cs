b0661343375140e379b460c513125d668a909a3a
Viktor Danchev <115632936+viktordanchev@users.noreply.github.com>
2023-02-12 18:45:55 +0200

    Update ProblemDescription.cs

--- 03.CSharp-Advanced/ExamPreparation/RetakeExam18August2022/01.BaristaContest/ProblemDescription.cs
+++ 03.CSharp-Advanced/ExamPreparation/RetakeExam18August2022/01.BaristaContest/ProblemDescription.cs
@@ -26,20 +26,20 @@
 //Finally, print the different coffee drinks that Nina managed to make and their count. 
 
 //Input
-//·	On the first line, you will receive the coffee quantities, separated by a comma and a single space (', '). 
-//·	On the second line, you will receive the milk quantities, separated by a comma and a single space (', ').
+//· On the first line, you will receive the coffee quantities, separated by a comma and a single space (', '). 
+//· On the second line, you will receive the milk quantities, separated by a comma and a single space (', ').
 
 //Output
-//·	On the first line – print whether Nina managed to use all of the coffee and milk:
+//· On the first line – print whether Nina managed to use all of the coffee and milk:
 //    o If she managed to use all of the coffee and all of the milk: "Nina is going to win! She used all the coffee and milk!"
 //    o If she didn't manage to use all of the coffee and milk: "Nina needs to exercise more! She didn't use all the coffee and milk!"
-//·	On the second line – print all of the coffee that you have left:
+//· On the second line – print all of the coffee that you have left:
 //    o If there is no coffee left: "Coffee left: none"
 //    o If there is coffee left: "Coffee left: {coffee1}, {coffee2}, {coffee3}, (…)"
-//·	On the third line – print all of the milk that you have left:
+//· On the third line – print all of the milk that you have left:
 //    o If there is no milk left: "Milk left: none"
 //    o If is milk left: "Milk left: {milk1}, {milk2}, {milk3}, (…)"
-//·	Then, you need to print the different types of coffee drinks, that were made, and their count. They must be ordered ascending by number and then sorted descending alphabetically.
+//· Then, you need to print the different types of coffee drinks, that were made, and their count. They must be ordered ascending by number and then sorted descending alphabetically.
 //    o	"Latte: {amount}"
 //    o	"Espresso: {amount}"
 //    o "Cortado: {amount}"
@@ -47,6 +47,6 @@
 //    o	"Americano: {amount}"
 
 //Constraints
-//·	All of the given numbers will be valid integers in the range [0…200].
-//·	There will be no case where the milk quantity reaches 0.
-//·	Do not use "\r\n" for a new line.
+//· All of the given numbers will be valid integers in the range [0…200].
+//· There will be no case where the milk quantity reaches 0.
+//· Do not use "\r\n" for a new line.
