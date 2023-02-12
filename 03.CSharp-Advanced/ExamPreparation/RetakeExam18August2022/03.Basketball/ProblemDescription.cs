a45c8b4e34024a78ad5f672d3c94fa8531bcb129
VDanchev <viktordan315@gmail.com>
2023-02-12 18:43:52 +0200

    .

--- 03.CSharp-Advanced/ExamPreparation/RetakeExam18August2022/03.Basketball/ProblemDescription.cs
+++ 03.CSharp-Advanced/ExamPreparation/RetakeExam18August2022/03.Basketball/ProblemDescription.cs
@@ -13,16 +13,16 @@
 //Player
 
 //You are given a class Player,  create the following properties:
-//•	Name: string
-//•	Position: string
-//•	Rating: double
-//•	Games: int
-//•	Retired: boolean – false by default
+//·	Name: string
+//·	Position: string
+//·	Rating: double
+//·	Games: int
+//·	Retired: boolean – false by default
 
 //The class constructor should receive(name, position, rating, games). 
 
 //The class should also have a method:
-//•	Override the ToString() method in the format:
+//·	Override the ToString() method in the format:
 //     "-Player: {name}
 //      --Position: {position}
 //      --Rating: {rating}
@@ -31,34 +31,34 @@
 //Team
 
 //Next, a class named Team is given, that has a collection (players) of type Player. All the entities of the Players collection have the same properties. The Team has also some additional properties:
-//•	Name: string
-//•	OpenPositions: int
-//•	Group: char
+//·	Name: string
+//·	OpenPositions: int
+//·	Group: char
 
 //The constructor of the Team class should receive the name, openPositions and group.
 
 //Implement the following features:
-//•	Getter Count - returns the count of the players in the team.
-//•	string AddPlayer(Player player) – adds a player to the team's collection, if there are open positions. Before adding a player, check:
+//·	Getter Count - returns the count of the players in the team.
+//·	string AddPlayer(Player player) – adds a player to the team's collection, if there are open positions. Before adding a player, check:
 //    o	If the name or position is null or empty, return "Invalid player's information.".
 //    o	If there are no more open positions, return "There are no more open positions.". 
 //    o	If the rating is under 80, return "Invalid player's rating.".
 //    o	Otherwise, return: "Successfully added {playerName} to the team. Remaining open positions: {openPositions}." and decrease the OpenPositions property of the team.
-//•	bool RemovePlayer(string name) – removes a player by given name.
+//·	bool RemovePlayer(string name) – removes a player by given name.
 //    o	If such exists, returns true;
 //    o Otherwise, returns false.
 
 //Note: Remember to update the OpenPositions property!
 
-//•	int RemovePlayerByPosition(string position) – removes all players by the given position.
+//·	int RemovePlayerByPosition(string position) – removes all players by the given position.
 //    o	If such exist(s), returns the count of the removed players;
 //    o Otherwise, returns 0.
 
 //Note: Remember to update the OpenPositions property!
 
-//•	Player RetirePlayer(string name) method – retire(set his Retired property to true, without removing him from the collection) the player with the given name, if he exists. As a result, return the player, or null, if he don't exist.
-//•	List<Player> AwardPlayers (int games) method – return a list with all players that have participated in games games or more.
-//•	Report() – returns a string with information about the team and players who are not retired in the following format:	
+//·	Player RetirePlayer(string name) method – retire(set his Retired property to true, without removing him from the collection) the player with the given name, if he exists. As a result, return the player, or null, if he don't exist.
+//·	List<Player> AwardPlayers (int games) method – return a list with all players that have participated in games games or more.
+//·	Report() – returns a string with information about the team and players who are not retired in the following format:	
 //    "Active players competing for Team {team} from Group {group}:
 //    {Player1}
 //    {Player2}
@@ -67,5 +67,5 @@
 //Note: Do not use "\n\r" for a new line.
 
 //Constraints
-//•	The names of the players will be always unique.
-//•	You will always have a player added before receiving methods, manipulating the teams' players.
+//·	The names of the players will be always unique.
+//·	You will always have a player added before receiving methods, manipulating the teams' players.
