efb5dbc73845cead76663d78cf28a0f96f92c662
VDanchev <viktordan315@gmail.com>
2023-02-15 15:57:54 +0200

    New Project Added

---
+++ 03.CSharp-Advanced/ExamPreparation/RetakeExam13April2022/01.MealPlan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.MealPlan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
