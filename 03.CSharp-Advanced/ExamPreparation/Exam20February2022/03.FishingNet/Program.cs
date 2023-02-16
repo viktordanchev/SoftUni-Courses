b5ea4525eb29e1c2eb30513c68ac145a56274426
VDanchev <viktordan315@gmail.com>
2023-02-16 09:49:34 +0200

    Few Projects Added

---
+++ 03.CSharp-Advanced/ExamPreparation/Exam20February2022/03.FishingNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.FishingNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
