0bceeb438caa139e4ab6761f48e4725741843ec7
VDanchev <viktordan315@gmail.com>
2023-02-26 10:30:25 +0200

    New Project Added

---
+++ 04.CSharp-OOP/01.Inheritance/01.InheritanceLab/03.HierarchicalInheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.HierarchicalInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
