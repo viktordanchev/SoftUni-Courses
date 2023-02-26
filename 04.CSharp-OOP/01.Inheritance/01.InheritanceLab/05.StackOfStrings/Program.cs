59766891bfd1b77cce67181aca9ed5b26b9dd2ce
VDanchev <viktordan315@gmail.com>
2023-02-26 10:31:32 +0200

    New Project Added

---
+++ 04.CSharp-OOP/01.Inheritance/01.InheritanceLab/05.StackOfStrings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _05.StackOfStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
