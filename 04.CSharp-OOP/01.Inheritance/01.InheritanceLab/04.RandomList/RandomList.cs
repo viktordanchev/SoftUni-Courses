b02c241752b0238a2099e3cec2148d901487ac7c
VDanchev <viktordan315@gmail.com>
2023-03-01 17:45:18 +0200

    Added Solutions

---
+++ 04.CSharp-OOP/01.Inheritance/01.InheritanceLab/04.RandomList/RandomList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random random = new Random();
+
+            return random.Next(0, Count).ToString();
+        }
+    }
+}
---
+++ 04.CSharp-OOP/01.Inheritance/01.InheritanceLab/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public Stack<string> AddRange(IEnumerable<string> collection)
+        {
+            foreach (string item in collection)
+            {
+                this.Push(item);
+            }
+
+            return this;
+        }
+    }
+}
