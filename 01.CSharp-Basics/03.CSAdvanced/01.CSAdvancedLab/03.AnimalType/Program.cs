deae7518e26c8c27d27534c4bb0e9bbc747f9c58
VDanchev <viktordan315@gmail.com>
2023-03-04 19:42:00 +0200

    New Project Added

---
+++ 01.CSharp-Basics/03.CSAdvanced/01.CSAdvancedLab/03.AnimalType/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.AnimalType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
