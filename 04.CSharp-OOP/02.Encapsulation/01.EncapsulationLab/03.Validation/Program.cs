3311e2c46409170da29e63f017588a5efe1c40cf
VDanchev <viktordan315@gmail.com>
2023-03-04 14:14:35 +0200

    New Project Added

---
+++ 04.CSharp-OOP/02.Encapsulation/01.EncapsulationLab/03.Validation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.Validation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
