343a3271abd9aff6391000400b1222e7337f0aa7
VDanchev <viktordan315@gmail.com>
2023-03-04 20:24:07 +0200

    New Project Added

---
+++ 01.CSharp-Basics/03.CSAdvanced/01.CSAdvancedLab/10.InvalidNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _10.InvalidNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 100 || number > 200 || number != 0)
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
