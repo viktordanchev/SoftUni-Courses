ffb89ccad985ac65972dc12d665950d821a10e60
VDanchev <viktordan315@gmail.com>
2023-03-06 11:14:17 +0200

    New Project Added

--- 02.CSharp-Fundamentals/01.Basics/02.BasicSyntaxConditionalStatementsAndLoopsExercise/09.PadawanEquipment/Program.cs
+++ 02.CSharp-Fundamentals/01.Basics/02.BasicSyntaxConditionalStatementsAndLoopsExercise/09.PadawanEquipment/Program.cs
@@ -6,7 +6,27 @@ namespace _09.PadawanEquipment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double money = double.Parse(Console.ReadLine());
+            int studentsCount = int.Parse(Console.ReadLine());
+            double lightsaberPrice = double.Parse(Console.ReadLine());
+            double robePrice = double.Parse(Console.ReadLine());
+            double beltPrice = double.Parse(Console.ReadLine());
+
+            double lightsabersSum = lightsaberPrice * (Math.Ceiling((double)studentsCount / 100 * 10) + studentsCount);
+            int freeBelts = studentsCount / 6;
+            double beltsSum = beltPrice * (studentsCount - freeBelts);
+            double robesSum = robePrice * studentsCount;
+
+            double result = lightsabersSum + robesSum + beltsSum;
+
+            if (money >= result)
+            {
+                Console.WriteLine($"The money is enough - it would cost {result:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"John will need {result - money:f2}lv more.");
+            }
         }
     }
 }
