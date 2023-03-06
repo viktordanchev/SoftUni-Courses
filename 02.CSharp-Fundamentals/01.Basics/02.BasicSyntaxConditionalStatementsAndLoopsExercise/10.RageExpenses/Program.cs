d2f36a8585f984e0f231da8d77ac05603526fb65
VDanchev <viktordan315@gmail.com>
2023-03-06 11:30:07 +0200

    New Project Added

--- 02.CSharp-Fundamentals/01.Basics/02.BasicSyntaxConditionalStatementsAndLoopsExercise/10.RageExpenses/Program.cs
+++ 02.CSharp-Fundamentals/01.Basics/02.BasicSyntaxConditionalStatementsAndLoopsExercise/10.RageExpenses/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace _10.RageExpenses
 {
@@ -6,7 +7,48 @@ namespace _10.RageExpenses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int lostGamesCount = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            int trashedHeadsetsCount = 0;
+            int trashedMousesCount = 0;
+            int trashedKeyboardsCount = 0;
+            int trashedDisplaysCount = 0;
+
+            for (int i = 1; i <= lostGamesCount; i++)
+            {
+                bool isHeadsetTrashed = false;
+                bool isMouseTrashed = false;
+
+                if (i % 2 == 0)
+                {
+                    isHeadsetTrashed = true;
+                    trashedHeadsetsCount++;
+                }
+
+                if (i % 3 == 0)
+                {
+                    isMouseTrashed = true;
+                    trashedMousesCount++;
+                }
+
+                if (isMouseTrashed && isHeadsetTrashed)
+                {
+                    trashedKeyboardsCount++;
+
+                    if (trashedKeyboardsCount % 2 == 0)
+                    {
+                        trashedDisplaysCount++;
+                    }
+                }
+            }
+
+            double expenses = trashedHeadsetsCount * headsetPrice + trashedMousesCount * mousePrice + trashedKeyboardsCount * keyboardPrice + trashedDisplaysCount * displayPrice;
+
+            Console.WriteLine($"Rage expenses: {expenses:f2} lv.");
         }
     }
 }
