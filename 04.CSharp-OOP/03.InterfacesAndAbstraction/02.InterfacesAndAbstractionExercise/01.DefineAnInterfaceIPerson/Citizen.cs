2f9d350c4f15fd2aabc5461b73c46bb9f2f59d27
VDanchev <viktordan315@gmail.com>
2023-03-10 17:31:10 +0200

    New solution added

---
+++ 04.CSharp-OOP/03.InterfacesAndAbstraction/02.InterfacesAndAbstractionExercise/01.DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,14 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        public Citizen(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; }
+        public int Age { get; }
+    }
+}
