3c59b0309a7dc44e4d13093e6ae10796aaa8f516
VDanchev <viktordan315@gmail.com>
2023-03-10 17:37:22 +0200

    New solution added

---
+++ 04.CSharp-OOP/03.InterfacesAndAbstraction/02.InterfacesAndAbstractionExercise/02.MultipleImplementation/Citizen.cs
@@ -0,0 +1,18 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            Name = name;
+            Age = age;
+            Id = id;
+            Birthdate = birthdate;
+        }
+
+        public string Name { get; }
+        public int Age { get; }
+        public string Id { get; }
+        public string Birthdate { get; }
+    }
+}
