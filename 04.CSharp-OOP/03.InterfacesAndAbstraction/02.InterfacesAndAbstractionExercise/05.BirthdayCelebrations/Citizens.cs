6eab20e2c74daa5d127c904d8ee5eba976d7786a
VDanchev <viktordan315@gmail.com>
2023-03-11 16:34:20 +0200

    New solution added

---
+++ 04.CSharp-OOP/03.InterfacesAndAbstraction/02.InterfacesAndAbstractionExercise/05.BirthdayCelebrations/Citizens.cs
@@ -0,0 +1,20 @@
+﻿using _05.BirthdayCelebrations.Interfaces;
+
+namespace _05.BirthdayCelebrations
+{
+    public class Citizens : IIdentifiable, INameable, IBirthable
+    {
+        public Citizens(string name, int age, string id, string birthdate)
+        {
+            Name = name;
+            Age = age;
+            Id = id;
+            Birthdate = birthdate;
+        }
+
+        public string Name { get; }
+        public int Age { get; }
+        public string Id { get; }
+        public string Birthdate { get; }
+    }
+}
---
+++ 04.CSharp-OOP/03.InterfacesAndAbstraction/02.InterfacesAndAbstractionExercise/05.BirthdayCelebrations/Robot.cs
@@ -0,0 +1,16 @@
+﻿using _05.BirthdayCelebrations.Interfaces;
+
+namespace _05.BirthdayCelebrations
+{
+    public class Robot : IIdentifiable
+    {
+        public Robot(string model, string id)
+        {
+            Model = model;
+            Id = id;
+        }
+
+        public string Model { get; }
+        public string Id { get; }
+    }
+}
