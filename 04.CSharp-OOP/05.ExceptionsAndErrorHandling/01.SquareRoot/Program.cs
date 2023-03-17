ef7e1ee24764db8c0db28e782d67daed3bbeb691
VDanchev <viktordan315@gmail.com>
2023-03-17 14:08:24 +0200

    New solution added

--- 04.CSharp-OOP/05.ExceptionsAndErrorHandling/01.SquareRoot/Program.cs
+++ 04.CSharp-OOP/05.ExceptionsAndErrorHandling/01.SquareRoot/Program.cs
@@ -6,7 +6,25 @@ namespace SquareRoot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number < 0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
         }
     }
 }
