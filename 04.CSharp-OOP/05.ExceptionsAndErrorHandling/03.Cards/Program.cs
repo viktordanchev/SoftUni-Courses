754030fbb81452fc6cddbbaa828370f4c806c1f3
VDanchev <viktordan315@gmail.com>
2023-03-17 10:23:44 +0200

    New project added

---
+++ 04.CSharp-OOP/05.ExceptionsAndErrorHandling/03.Cards/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
