3541db5d06dd30c3bfc6c059dd9c43ceda94e162
VDanchev <viktordan315@gmail.com>
2023-03-20 18:03:06 +0200

    Update

--- 04.CSharp-OOP/05.ExceptionsAndErrorHandling/04.SumOfIntegers/Program.cs
+++ 04.CSharp-OOP/05.ExceptionsAndErrorHandling/04.SumOfIntegers/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace SumOfIntegers
 {
@@ -7,17 +6,7 @@ namespace SumOfIntegers
     {
         static void Main(string[] args)
         {
-            Dictionary<int, string> map = new Dictionary<int, string>()
-            {
-                [1] = "a",
-                [2] = "b",
-                [3] = "c"
-            };
-
-            map.Remove(2);
-            map.Add(4, "da");
-
-            Console.WriteLine();
+            Console.WriteLine("Hello World!");
         }
     }
 }
