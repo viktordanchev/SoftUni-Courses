00717fab672179969d3cbedc2e760171c124b304
VDanchev <viktordan315@gmail.com>
2023-03-21 18:42:36 +0200

    Update

--- 04.CSharp-OOP/06.ReflectionAndAttributes/01.ReflectionAndAttributesLab/01.Stealer/Program.cs
+++ 04.CSharp-OOP/06.ReflectionAndAttributes/01.ReflectionAndAttributesLab/01.Stealer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace _01.Stealer
 {
@@ -6,7 +7,47 @@ namespace _01.Stealer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Type type = typeof(Da);
+
+            ConstructorInfo[] constructors = type.GetConstructors();
+
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                ParameterInfo[] parameters = constructor.GetParameters();
+
+                object[] values = new object[parameters.Length];
+
+                int index = 0;
+                foreach (ParameterInfo value in parameters)
+                {
+                    values[index++] = GetDefault(value.ParameterType);
+                }
+
+                Da da = Activator.CreateInstance(type, values) as Da;
+            }
         }
+
+        static object GetDefault(Type type)
+        {
+            if (type.IsValueType)
+                return Activator.CreateInstance(type);
+
+            return null;
+        }
+    }
+
+    class Da
+    {
+        private int id;
+        private int age;
+
+        public Da(int id, int age)
+        {
+            Id = id;
+            Age = age;
+        }
+
+        public int Id { get => 1; set { id = value; } }
+        public int Age { get => 5; set { age = value; } }
     }
 }
