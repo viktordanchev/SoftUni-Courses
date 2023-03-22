de1b3dcf59b763e9f5bff15eafc9ae33f195c9d7
VDanchev <viktordan315@gmail.com>
2023-03-22 17:02:45 +0200

    New solution added

---
+++ 04.CSharp-OOP/06.ReflectionAndAttributes/01.ReflectionAndAttributesLab/05.CreateAttribute/AuthorAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [AttributeUsage((AttributeTargets)68, AllowMultiple = true)]
+    public class AuthorAttribute : Attribute
+    {
+        public AuthorAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+}
---
+++ 04.CSharp-OOP/06.ReflectionAndAttributes/01.ReflectionAndAttributesLab/05.CreateAttribute/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthorProblem
+{
+    [Author("Victor")]
+    class StartUp
+    {
+        [Author("George")]
+        static void Main(string[] args)
+        {
+        }
+    }
+}
