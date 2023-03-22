2403dd963831d7028d4a4cbf339f5431b495e17c
VDanchev <viktordan315@gmail.com>
2023-03-22 17:02:49 +0200

    New solution added

---
+++ 04.CSharp-OOP/06.ReflectionAndAttributes/01.ReflectionAndAttributesLab/06.CodeTracker/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace AuthorProblem
+{
+    [Author("Victor")]
+    class StartUp
+    {
+        [Author("George")]
+        static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
---
+++ 04.CSharp-OOP/06.ReflectionAndAttributes/01.ReflectionAndAttributesLab/06.CodeTracker/Tracker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+            MethodInfo[] methods = type.GetMethods((BindingFlags)28);
+
+            foreach (MethodInfo method in methods)
+            {
+                if (method.CustomAttributes.Any(n => n.AttributeType == typeof(AuthorAttribute)))
+                {
+                    AuthorAttribute[] attributes = method.GetCustomAttributes<AuthorAttribute>().ToArray();
+
+                    foreach (AuthorAttribute attribute in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is wirtten by {attribute.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
