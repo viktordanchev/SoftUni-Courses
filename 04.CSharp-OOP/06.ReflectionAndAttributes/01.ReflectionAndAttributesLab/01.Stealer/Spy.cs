16f286b285cb6b68bc1771ff282718f734b34766
VDanchev <viktordan315@gmail.com>
2023-03-22 14:37:09 +0200

    New solution added

---
+++ 04.CSharp-OOP/06.ReflectionAndAttributes/01.ReflectionAndAttributesLab/01.Stealer/Spy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fieldsNames)
+        {
+            Type type = Type.GetType(className);
+            FieldInfo[] fieldsInfo = type.GetFields((BindingFlags)60);
+            StringBuilder sb = new();
+
+            Object obj = Activator.CreateInstance(type, new object[] { });
+
+            sb.AppendLine($"Class under investigation: {className}");
+            foreach (FieldInfo field in fieldsInfo.Where(f => fieldsNames.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(obj)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ 04.CSharp-OOP/06.ReflectionAndAttributes/01.ReflectionAndAttributesLab/02.HighQualityMistakes/Spy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fieldsNames)
+        {
+            Type type = Type.GetType(className);
+            FieldInfo[] fieldsInfo = type.GetFields((BindingFlags)60);
+            Object obj = Activator.CreateInstance(type);
+            StringBuilder sb = new();
+
+            sb.AppendLine($"Class under investigation: {className}");
+            foreach (FieldInfo field in fieldsInfo.Where(f => fieldsNames.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(obj)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
