26b1b65a23a5e6492fee21306d5ec540064be7a4
VDanchev <viktordan315@gmail.com>
2023-03-22 16:05:26 +0200

    New solution added

---
+++ 04.CSharp-OOP/06.ReflectionAndAttributes/01.ReflectionAndAttributesLab/04.Collector/Spy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string CollectGettersAndSetters(string className)
+        {
+            Type type = Type.GetType(className);
+            MethodInfo[] methods = type.GetMethods((BindingFlags)54);
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var method in methods.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+            foreach (var method in methods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} will set field of {method.ReturnType}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
