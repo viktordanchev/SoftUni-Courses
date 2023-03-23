c6ba5abeba07a36e61105bb082c0089224e7eb22
VDanchev <viktordan315@gmail.com>
2023-03-23 19:03:35 +0200

    .

---
+++ 04.CSharp-OOP/07.UnitTesting/01.UnitTestingLab/Tests/AxeTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        [Test]
+        public void AxeLoosesDurabilityAfterAttack()
+        {
+            Axe axe = new(10, 10);
+            Dummy dummy = new(10, 10);
+
+            axe.Attack(dummy);
+
+            Assert.That(axe.DurabilityPoints, Is.EqualTo(9), "Axe Durability doesn't change after attack.");
+        }
+    }
+}
