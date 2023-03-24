bc99acd6201691e7c00a9d8cfc2ef39b3cba9a19
VDanchev <viktordan315@gmail.com>
2023-03-24 21:12:41 +0200

    Added new solution

--- 04.CSharp-OOP/07.UnitTesting/02.UnitTestingExercises/Database.Tests/DatabaseTests.cs
+++ 04.CSharp-OOP/07.UnitTesting/02.UnitTestingExercises/Database.Tests/DatabaseTests.cs
@@ -1,18 +1,46 @@
 using NUnit.Framework;
+using System;
 
-namespace DatabaseTests
+namespace Database.Tests
 {
     public class Tests
     {
+        private Database database;
+        Database fullDatabase;
+        private int indexes;
+
         [SetUp]
         public void Setup()
         {
+            indexes = 0;
+            database = new(new int[indexes]);
+            fullDatabase = new(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 });
         }
 
         [Test]
-        public void Test1()
+        public void DatabaseConstructorShouldSetDataProperly()
         {
-            Assert.Pass();
+            database = new(new int[indexes + 5]);
+
+            Assert.AreEqual(5, database.Count);
+        }
+
+        [Test]
+        public void TryToAddMoreThan16ElementsToArray()
+        {
+            Assert.Throws<InvalidOperationException>(() => fullDatabase.Add(17));
+        }
+
+        [Test]
+        public void TryToRemoveInEmptyArray()
+        {
+            Assert.Throws<InvalidOperationException>(() => database.Remove());
+        }
+
+        [Test]
+        public void FetchMethodShouldReturnElementsInArray()
+        {
+            Assert.AreEqual(16, fullDatabase.Count);
         }
     }
 }
