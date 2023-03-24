4935773b914557cf393a7f21bbc616527fe6ab67
VDanchev <viktordan315@gmail.com>
2023-03-24 20:27:31 +0200

    Renamed namespaces

--- 04.CSharp-OOP/07.UnitTesting/01.UnitTestingLab/Axe.Tests/AxeTests.cs
+++ 04.CSharp-OOP/07.UnitTesting/01.UnitTestingLab/Axe.Tests/AxeTests.cs
@@ -1,7 +1,7 @@
 using NUnit.Framework;
 using System;
 
-namespace Tests
+namespace AxeTests
 {
     [TestFixture]
     public class AxeTests
--- 04.CSharp-OOP/07.UnitTesting/01.UnitTestingLab/Dummy.Tests/DummyTests.cs
+++ 04.CSharp-OOP/07.UnitTesting/01.UnitTestingLab/Dummy.Tests/DummyTests.cs
@@ -1,7 +1,7 @@
 using NUnit.Framework;
 using System;
 
-namespace Tests
+namespace DummyTests
 {
     public class Tests
     {
