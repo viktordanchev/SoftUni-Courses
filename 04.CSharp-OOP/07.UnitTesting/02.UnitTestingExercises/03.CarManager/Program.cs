d5d38d267e9e527e3ffa88bcf29b4496f1f8453e
VDanchev <viktordan315@gmail.com>
2023-03-24 20:26:37 +0200

    Added new projects

---
+++ 04.CSharp-OOP/07.UnitTesting/02.UnitTestingExercises/03.CarManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.CarManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 04.CSharp-OOP/07.UnitTesting/02.UnitTestingExercises/FightingArena.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace FightingArena.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
