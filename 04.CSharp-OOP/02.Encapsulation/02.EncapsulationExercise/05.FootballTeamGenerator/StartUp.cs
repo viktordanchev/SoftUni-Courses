707c35cfe47621c2e07cb7f69c8a4b6db65fb9b7
VDanchev <viktordan315@gmail.com>
2023-03-28 17:10:09 +0300

    Added new solution

---
+++ 04.CSharp-OOP/02.Encapsulation/02.EncapsulationExercise/05.FootballTeamGenerator/StartUp.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FootballTeamGenerator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var teams = new List<Team>();
+
+            string input = Console.ReadLine();
+
+            while(input != "END")
+            {
+                string[] data = input.Split(';', StringSplitOptions.RemoveEmptyEntries);
+                string command = data[0];
+                string teamName = data[1];
+
+                try
+                {
+                    switch (command)
+                    {
+                        case "Team":
+                            Team newTeam = new(teamName);
+                            teams.Add(newTeam);
+                            break;
+                        case "Add":
+                            string playerName = data[2];
+                            int endurance = int.Parse(data[3]);
+                            int sprint = int.Parse(data[4]);
+                            int dribble = int.Parse(data[5]);
+                            int passing = int.Parse(data[6]);
+                            int shooting = int.Parse(data[7]);
+
+                            Team team = GetTeam(teams, teamName);
+                            team.AddPlayer(new Player(playerName, endurance, sprint, dribble, passing, shooting));
+                            break;
+                        case "Remove":
+                            playerName = data[2];
+
+                            team = GetTeam(teams, teamName);
+                            team.RemovePlayer(playerName);
+                            break;
+                        case "Rating":
+                            team = GetTeam(teams, teamName);
+                            Console.WriteLine($"{teamName} - {team.Rating:f0}");
+                            break;
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+
+        static Team GetTeam(List<Team> teams, string teamName)
+        {
+            Team currTeam = teams.FirstOrDefault(t => t.Name == teamName);
+
+            if (currTeam == null)
+            {
+                throw new ArgumentException($"Team {teamName} does not exist.");
+            }
+
+            return currTeam;
+        }
+    }
+}
---
+++ 04.CSharp-OOP/02.Encapsulation/02.EncapsulationExercise/05.FootballTeamGenerator/Team.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FootballTeamGenerator
+{
+    public class Team
+    {
+        private List<Player> team;
+        private string name;
+
+        public Team(string name)
+        {
+            Name = name;
+            team = new List<Player>();
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("A name should not be empty.");
+
+                name = value;
+            }
+        }
+        public double Rating
+        {
+            get
+            {
+                double rating = 0;
+
+                foreach (var player in team)
+                {
+                    rating += player.OverallSkillLevel;
+                }
+
+                if (team.Count > 0)
+                    rating /= team.Count;
+
+                return rating;
+            }
+        }
+        public int NumberOfPlayers { get => team.Count; }
+
+        public void AddPlayer(Player player)
+        {
+            team.Add(player);
+        }
+
+        public void RemovePlayer(string playerName)
+        {
+            if (!team.Any(p => p.Name == playerName))
+                throw new ArgumentException($"Player {playerName} is not in {Name} team.");
+
+            Player player = team.First(p => p.Name == playerName);
+
+            team.Remove(player);
+        }
+    }
+}
