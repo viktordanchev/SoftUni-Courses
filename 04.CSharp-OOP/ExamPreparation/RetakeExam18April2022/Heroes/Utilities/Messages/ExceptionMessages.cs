41fa5f0ef595c22601db67fee114bdfb5f54f431
VDanchev <viktordan315@gmail.com>
2023-04-01 19:58:04 +0300

    New project added

---
+++ 04.CSharp-OOP/ExamPreparation/RetakeExam18April2022/Heroes/Utilities/Messages/ExceptionMessages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Utilities.Messages
+{
+    public static class ExceptionMessages
+    {
+        public const string WeaponTypeNull = "Weapon type cannot be null or empty.";
+
+        public const string DurabilityBelowZero = "Durability cannot be below 0.";
+
+        public const string HeroNameNull = "Hero name cannot be null or empty.";
+
+        public const string HeroHealthBelowZero = "Hero health cannot be below 0.";
+
+        public const string HeroArmourBelowZero = "Hero armour cannot be below 0.";
+
+        public const string WeaponNull = "Weapon cannot be null.";
+    }
+}
---
+++ 04.CSharp-OOP/ExamPreparation/RetakeExam18April2022/Heroes/Utilities/Messages/OutputMessages.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Utilities.Messages
+{
+    public static class OutputMessages
+    {
+        public const string MapFightKnightsWin = "The knights took {0} casualties but won the battle.";
+
+        public const string MapFigthBarbariansWin = "The barbarians took {0} casualties but won the battle.";
+
+        public const string HeroAlreadyExist = "The hero {0} already exists.";
+
+        public const string HeroTypeIsInvalid = "Invalid hero type.";
+
+        public const string SuccessfullyAddedKnight = "Successfully added Sir {0} to the collection.";
+
+        public const string SuccessfullyAddedBarbarian = "Successfully added Barbarian {0} to the collection.";
+
+        public const string WeaponAlreadyExists = "The weapon {0} already exists.";
+
+        public const string WeaponTypeIsInvalid = "Invalid weapon type.";
+
+        public const string WeaponAddedSuccessfully = "A {0} {1} is added to the collection.";
+
+        public const string HeroDoesNotExist = "Hero {0} does not exist.";
+
+        public const string WeaponDoesNotExist = "Weapon {0} does not exist.";
+
+        public const string HeroAlreadyHasWeapon = "Hero {0} is well-armed.";
+
+        public const string WeaponAddedToHero = "Hero {0} can participate in battle using a {1}.";
+    }
+}
