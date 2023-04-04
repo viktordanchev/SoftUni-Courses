ec8c4cf86b6ca22cbdc61ab8f1690a0b07ee37dc
VDanchev <viktordan315@gmail.com>
2023-04-04 11:21:37 +0300

    New project tests added

---
+++ 04.CSharp-OOP/ExamPreparation/Exam09April2022/SmartphoneShop.Tests/SmartphoneShopTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace SmartphoneShop.Tests
+{
+    [TestFixture]
+    public class SmartphoneShopTests
+    {
+        [SetUp]
+        public void SetUp() 
+        {
+
+        }
+
+        [Test]
+        public void Test()
+        {
+
+        }
+    }
+}
