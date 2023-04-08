7b2b1e059ec59d7373dacc3f077942bd709c5dec
Viktor Danchev <viktordan315@gmail.com>
2023-04-08 20:11:11 +0300

    New solution added

--- 02.CSharp-Fundamentals/ExamPreparation/01.ProgrammingFundamentalsFinalExamRetake/01.TheImitationGame/Program.cs
+++ 02.CSharp-Fundamentals/ExamPreparation/01.ProgrammingFundamentalsFinalExamRetake/01.TheImitationGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Intrinsics.Arm;
 
 namespace _01.TheImitationGame
 {
@@ -6,7 +7,45 @@ namespace _01.TheImitationGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string message = Console.ReadLine();
+
+            string input = Console.ReadLine();
+            while (input != "Decode")
+            {
+                string[] data = input.Split('|', StringSplitOptions.RemoveEmptyEntries);
+                string command = data[0];
+
+                switch (command)
+                {
+                    case "Move":
+                        int numberOfLetters = int.Parse(data[1]);
+                        message = Move(message, numberOfLetters);
+                        break;
+                    case "Insert":
+                        int index = int.Parse(data[1]);
+                        string value = data[2];
+                        message = message.Insert(index, value);
+                        break;
+                    case "ChangeAll":
+                        string substring = data[1];
+                        string replacement = data[2];
+                        message = message.Replace(substring, replacement);
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"The decrypted message is: {message}");
+        }
+
+        static string Move(string message, int numberOfLetters)
+        {
+            string rem = message.Substring(0, numberOfLetters);
+            message = message.Remove(0, numberOfLetters);
+            message = message.Insert(message.Length, rem);
+
+            return message;
         }
     }
 }
