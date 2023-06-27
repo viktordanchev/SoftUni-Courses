6e91a7671cc12c4a6acb37d8d4b577648741a2fc
VDanchev <viktordan315@gmail.com>
2023-06-27 22:35:56 +0300

    Added new solution

---
+++ JS Front-End/01.JSSyntaxFundamentals/01.JSSyntaxFundamentalsLab/04.MonthPrinter/month-printer.js
@@ -0,0 +1,46 @@
+function printMonth(num) {
+    let month = 'Error!';
+
+    switch (num) {
+        case 1:
+            month = 'January'
+            break;
+        case 2:
+            month = 'February'
+            break;
+        case 3:
+            month = 'March'
+            break;
+        case 4:
+            month = 'April'
+            break;
+        case 5:
+            month = 'May'
+            break;
+        case 6:
+            month = 'June'
+            break;
+        case 7:
+            month = 'July'
+            break;
+        case 8:
+            month = 'August'
+            break;
+        case 9:
+            month = 'September'
+            break;
+        case 10:
+            month = 'October'
+            break;
+        case 11:
+            month = 'November'
+            break;
+        case 12:
+            month = 'December'
+            break;
+    }
+    
+    console.log(month);
+}
+
+printMonth(3);
