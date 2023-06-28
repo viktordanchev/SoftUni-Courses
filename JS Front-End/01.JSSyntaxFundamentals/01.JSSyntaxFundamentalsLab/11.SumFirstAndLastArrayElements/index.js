0eb929cf6a894e336f237248331b3e2d7cc64436
VDanchev <viktordan315@gmail.com>
2023-06-28 18:08:44 +0300

    Added new solution

---
+++ JS Front-End/01.JSSyntaxFundamentals/01.JSSyntaxFundamentalsLab/11.SumFirstAndLastArrayElements/index.js
@@ -0,0 +1,7 @@
+function sumFirstAndLastElements(elements) {
+  const firstElement = elements[0];
+  const lastElement = elements[elements.length - 1];
+  console.log(firstElement + lastElement);
+}
+
+sumFirstAndLastElements([20, 30, 40]);
