f88ec0122693c8a28d1f2c8efb7fd0c2bb05a88c
VDanchev <viktordan315@gmail.com>
2023-06-28 18:08:33 +0300

    Added new solution

---
+++ JS Front-End/01.JSSyntaxFundamentals/01.JSSyntaxFundamentalsLab/06.LargestNumber/index.js
@@ -0,0 +1,6 @@
+function findBiggestNum(firstNum, secondNum, thirdNum) {
+  const largestNumber = Math.max(firstNum, secondNum, thirdNum);
+  console.log(`The largest number is ${largestNumber}.`);
+}
+
+findBiggestNum(5, -3, 16);
