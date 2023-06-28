b9580e7739f24464cd1384dd266a78946b9823da
VDanchev <viktordan315@gmail.com>
2023-06-28 18:08:35 +0300

    Added new solution

---
+++ JS Front-End/01.JSSyntaxFundamentals/01.JSSyntaxFundamentalsLab/07.TheatrePromotions/index.js
@@ -0,0 +1,37 @@
+function calculatePrice(dayType, age) {
+  let price = "Error!";
+
+  switch (dayType) {
+    case "Weekday":
+      if (age >= 0 && age <= 18) {
+        price = "12$";
+      } else if (age > 18 && age <= 64) {
+        price = "18$";
+      } else if (age > 64 && age <= 122) {
+        price = "12$";
+      }
+      break;
+    case "Weekend":
+      if (age >= 0 && age <= 18) {
+        price = "15$";
+      } else if (age > 18 && age <= 64) {
+        price = "20$";
+      } else if (age > 64 && age <= 122) {
+        price = "15$";
+      }
+      break;
+    case "Holiday":
+      if (age >= 0 && age <= 18) {
+        price = "5$";
+      } else if (age > 18 && age <= 64) {
+        price = "12$";
+      } else if (age > 64 && age <= 122) {
+        price = "10$";
+      }
+      break;
+  }
+
+  console.log(price);
+}
+
+calculatePrice("Weekday", 42);
