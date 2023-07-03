e0348faa28365983274e1ae4bc04ed20f2a433a2
VDanchev <viktordan315@gmail.com>
2023-07-03 18:40:44 +0300

    Added new solution

---
+++ JS Front-End/02.FunctionsAndStatements/02.FunctionsAndStatementsExercises/01.SmallestOfThreeNumbers/index.js
@@ -0,0 +1,8 @@
+function printSmallestNumber(...numbers) {
+  console.log(Math.min(...numbers));
+}
+
+printSmallestNumber(2, 5, 3);
+printSmallestNumber(600, 342, 123);
+printSmallestNumber(25, 21, 4);
+printSmallestNumber(2, 2, 2);
