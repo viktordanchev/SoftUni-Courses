324b194631f280ea43b7c8f7ffb58fc2ff6cc480
VDanchev <viktordan315@gmail.com>
2023-07-07 11:54:28 +0300

    Added new solution

--- JS Front-End/03.ObjectsAndClasses/01.ObjectsAndClassesLab/06.PhoneBook/index.js
+++ JS Front-End/03.ObjectsAndClasses/01.ObjectsAndClassesLab/06.PhoneBook/index.js
@@ -1,8 +1,24 @@
-function printNameAndPhone(arr) {}
+function printNameAndPhone(arr) {
+  const phoneBook = {};
+  for (let index = 0; index < arr.length; index++) {
+    const [name, number] = arr[index].split(" ");
+    phoneBook[name] = number;
+  }
+
+  for (const contact in phoneBook) {
+    console.log(`${contact} -> ${phoneBook[contact]}`);
+  }
+}
 
 printNameAndPhone([
   "Tim 0834212554",
   "Peter 0877547887",
   "Bill 0896543112",
   "Tim 0876566344",
+]);
+printNameAndPhone([
+  "George 0552554",
+  "Peter 087587",
+  "George 0453112",
+  "Bill 0845344",
 ]);
