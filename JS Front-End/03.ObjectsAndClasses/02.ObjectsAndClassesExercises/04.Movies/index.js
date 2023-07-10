efedb5b0a8c021f1ddf827ef79f455f8c12dfd79
VDanchev <viktordan315@gmail.com>
2023-07-10 17:30:35 +0300

    Added new solution

---
+++ JS Front-End/03.ObjectsAndClasses/02.ObjectsAndClassesExercises/04.Movies/index.js
@@ -0,0 +1,32 @@
+function solve(arr) {
+  class Movie {
+    constructor(name, director, date) {
+      name, director, date;
+    }
+  }
+  const movies = {};
+
+  while (arr.length > 0) {
+    const line = arr.shift().split(" ");
+    const command = line[0];
+  }
+}
+
+solve([
+  "addMovie Fast and Furious",
+  "addMovie Godfather",
+  "Inception directedBy Christopher Nolan",
+  "Godfather directedBy Francis Ford Coppola",
+  "Godfather onDate 29.07.2018",
+  "Fast and Furious onDate 30.07.2018",
+  "Batman onDate 01.08.2018",
+  "Fast and Furious directedBy Rob Cohen",
+]);
+solve([
+  "addMovie The Avengers",
+  "addMovie Superman",
+  "The Avengers directedBy Anthony Russo",
+  "The Avengers onDate 30.07.2010",
+  "Captain America onDate 30.07.2010",
+  "Captain America directedBy Joe Russo",
+]);
