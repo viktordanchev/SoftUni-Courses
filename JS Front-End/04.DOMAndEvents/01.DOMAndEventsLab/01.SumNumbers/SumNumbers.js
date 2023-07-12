dc39e313c97c403f761606fabb660f707915b918
VDanchev <viktordan315@gmail.com>
2023-07-12 19:21:24 +0300

    Added new solution

---
+++ JS Front-End/04.DOMAndEvents/01.DOMAndEventsLab/01.SumNumbers/SumNumbers.js
@@ -0,0 +1,6 @@
+function calc() {
+  const firstNum = Number(document.getElementById("num1").value);
+  const secondNum = Number(document.getElementById("num2").value);
+
+  document.getElementById("sum").value = firstNum + secondNum;
+}
