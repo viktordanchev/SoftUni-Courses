401fe1ba77721c730f433a6417955eec0651a37f
VDanchev <viktordan315@gmail.com>
2023-07-13 22:48:30 +0300

    Added new solution

---
+++ JS Front-End/04.DOMAndEvents/01.DOMAndEventsLab/02.ShowMore/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+  const elementToHide = document.getElementById("more");
+  const elementToShow = document.getElementById("text");
+
+  elementToHide.style.display = "none";
+  elementToShow.style.display = "inline";
+}
