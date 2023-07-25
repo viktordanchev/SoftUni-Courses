e90aa87d5a134d8a59a07f486364d8846ba336d6
VDanchev <viktordan315@gmail.com>
2023-07-25 20:06:11 +0300

    Added new solution

--- JS Front-End/05.HTTPAndREST/01.HTTPAndRESTExercises/01.BusStop/app.js
+++ JS Front-End/05.HTTPAndREST/01.HTTPAndRESTExercises/01.BusStop/app.js
@@ -1,9 +1,27 @@
 function getInfo() {
   const busID = document.getElementById("stopId").value;
+  getRepo(busID);
 
-  const repo = fetch(
-    `http://localhost:3030/jsonstore/bus/businfo/${busID}`
-  ).then((repo) => repo.json());
+  function getRepo(busID) {
+    const stopName = document.getElementById("stopName");
 
-  console.log(repo);
+    fetch(`http://localhost:3030/jsonstore/bus/businfo/${busID}`)
+      .then((res) => res.json())
+      .then((data) => {
+        stopName.textContent = data["name"];
+        const keys = Object.keys(data["buses"]);
+
+        for (const key of keys) {
+          const li = document
+            .getElementById("buses")
+            .appendChild(document.createElement("li"));
+
+          li.textContent = `Bus ${key} arrives in ${data["buses"][key]} minutes`;
+        }
+      })
+      .catch(() => stopName.textContent = "Error");
+
+    document.getElementById("stopId").value = "";
+    document.getElementById("buses").innerHTML = "";
+  }
 }
