79cb92e05375f489b599fb76a6a7ee0b73e3c137
VDanchev <viktordan315@gmail.com>
2023-07-25 20:06:18 +0300

    Added new solution

--- JS Front-End/05.HTTPAndREST/01.HTTPAndRESTExercises/02.BusSchedule/app.js
+++ JS Front-End/05.HTTPAndREST/01.HTTPAndRESTExercises/02.BusSchedule/app.js
@@ -1,16 +1,34 @@
 function solve() {
-    function depart() {
-        // TODO: 
-    }
+  let ID = "depot";
+  let stopName = "";
+  const spanInfo = document.querySelector("span.info");
+  const departButton = document.getElementById("depart");
+  const arriveButton = document.getElementById("arrive");
 
-    async function arrive() {
-        // TODO:
-    }
+  function depart() {
+    fetch(`http://localhost:3030/jsonstore/bus/schedule/${ID}`)
+      .then((res) => res.json())
+      .then((data) => {
+        stopName = data["name"];
+        ID = data["next"];
 
-    return {
-        depart,
-        arrive
-    };
+        spanInfo.textContent = `Next stop ${stopName}`;
+      });
+
+    departButton.disabled = true;
+    arriveButton.disabled = false;
+  }
+
+  async function arrive() {
+    spanInfo.textContent = `Arriving at ${stopName}`;
+    departButton.disabled = false;
+    arriveButton.disabled = true;
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
