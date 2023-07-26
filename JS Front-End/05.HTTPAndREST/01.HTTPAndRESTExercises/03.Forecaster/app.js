51940a327c5177e517edf7f89e1b813b07e788eb
VDanchev <viktordan315@gmail.com>
2023-07-26 20:13:37 +0300

    Added new solution

--- JS Front-End/05.HTTPAndREST/01.HTTPAndRESTExercises/03.Forecaster/app.js
+++ JS Front-End/05.HTTPAndREST/01.HTTPAndRESTExercises/03.Forecaster/app.js
@@ -11,6 +11,7 @@ function attachEvents() {
           if (currLocation["name"] === location) {
             document.getElementById("forecast").style = "display:block";
             displayWeatherToday(currLocation["code"]);
+            displayWeatherThreeDayForecast(currLocation["code"]);
           }
         }
       });
@@ -18,32 +19,97 @@ function attachEvents() {
 
   async function displayWeatherToday(code) {
     try {
-      await (
+      const res = await (
         await fetch(`http://localhost:3030/jsonstore/forecaster/today/${code}`)
       ).json();
 
-      document
-        .getElementById("current")
-        .appendChild(document.createElement("div"))
-        .classList.add("forecasts");
+      const resData = {
+        0: res["name"],
+        1: `${res["forecast"]["low"]}°/${res["forecast"]["high"]}°`,
+        2: res["forecast"]["condition"],
+      };
 
-      const forecasts = document.querySelector("div.forecasts");
+      const forecasts = document
+        .getElementById("current")
+        .appendChild(document.createElement("div"));
+
+      forecasts.className = "forecasts";
 
       forecasts
         .appendChild(document.createElement("span"))
         .classList.add("condition", "symbol");
 
-      forecasts
-        .appendChild(document.createElement("span"))
-        .classList.add("condition");
+      const spanSymbol = document.querySelector("span.symbol");
+      spanSymbol.textContent = symbol[resData[2]];
 
-      const spanCondition = document.querySelector("span.condition");
+      forecasts.appendChild(document.createElement("span")).className =
+        "condition";
 
-      console.log(spanCondition);
+      const spanCondition = document.querySelectorAll("span.condition")[1];
 
-      spanCondition.appendChild(document.createElement("span"));
+      for (let index = 0; index < 3; index++) {
+        spanCondition.appendChild(document.createElement("span")).className =
+          "forecast-data";
+      }
+
+      const spanForecastData = document.querySelectorAll("span.forecast-data");
+
+      for (let index = 0; index < 3; index++) {
+        spanForecastData[index].textContent = resData[index];
+      }
     } catch (error) {}
   }
+
+  async function displayWeatherThreeDayForecast(code) {
+    try {
+      const res = await (
+        await fetch(
+          `http://localhost:3030/jsonstore/forecaster/upcoming/${code}`
+        )
+      ).json();
+
+      const upcoming = document
+        .getElementById("upcoming")
+        .appendChild(document.createElement("div"));
+
+      upcoming.className = "forecast-info";
+
+      for (let index = 0; index < 3; index++) {
+        const resData = res["forecast"][index];
+
+        const spanUpcoming = upcoming.appendChild(
+          document.createElement("span")
+        );
+        spanUpcoming.className = "upcoming";
+
+        const spanSymbol = spanUpcoming.appendChild(
+          document.createElement("span")
+        );
+        spanSymbol.className = "symbol";
+        spanSymbol.textContent = symbol[resData["condition"]];
+
+        const resDataObject = {
+          0: `${resData["low"]}°/${resData["high"]}°`,
+          1: resData["condition"],
+        };
+
+        for (let index = 0; index < 2; index++) {
+          const spanForecastDataTemperature = spanUpcoming.appendChild(
+            document.createElement("span")
+          );
+          spanForecastDataTemperature.className = "forecast-data";
+          spanForecastDataTemperature.textContent = resDataObject[index];
+        }
+      }
+    } catch (error) {}
+  }
+
+  const symbol = {
+    Sunny: "\u2600",
+    "Partly sunny": "\u26C5",
+    Overcast: "\u2601",
+    Rain: "\u2614",
+  };
 }
 
 attachEvents();
