d8f8cb9150661f330b0c93d3ed52a848096bfeee
VDanchev <viktordan315@gmail.com>
2023-07-28 21:21:15 +0300

    Added new solution

--- JS Front-End/05.HTTPAndREST/01.HTTPAndRESTExercises/07.Students/app.js
+++ JS Front-End/05.HTTPAndREST/01.HTTPAndRESTExercises/07.Students/app.js
@@ -1,5 +1,56 @@
 function attachEvents() {
-  // TODO:
+  const url = "http://localhost:3030/jsonstore/collections/students";
+  const tbody = document.querySelector("#results tbody");
+
+  document.getElementById("submit").addEventListener("click", submit);
+
+  function submit() {
+    const firstName = document.getElementsByName("firstName")[0].value;
+    const lastName = document.getElementsByName("lastName")[0].value;
+    const facultyNumber = document.getElementsByName("facultyNumber")[0].value;
+    const grade = document.getElementsByName("grade")[0].value;
+
+    if (firstName && lastName && facultyNumber && grade) {
+      addStudentToData({
+        firstName: firstName,
+        lastName: lastName,
+        facultyNumber: facultyNumber,
+        grade: grade,
+      });
+    }
+
+    Array.from(document.querySelectorAll(".inputs input")).forEach(
+      (input) => (input.value = "")
+    );
+
+    tbody.innerHTML = "";
+    fillTheTable();
+  }
+
+  async function addStudentToData(student) {
+    await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(student),
+    });
+  }
+
+  async function fillTheTable() {
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => {
+        for (const key in res) {
+          const tr = tbody.appendChild(document.createElement("tr"));
+          const student = res[key];
+
+          for (const key in student) {
+            if (key !== "_id") {
+              tr.appendChild(document.createElement("td")).textContent =
+                student[key];
+            }
+          }
+        }
+      });
+  }
 }
 
 attachEvents();
--- JS Front-End/05.HTTPAndREST/01.HTTPAndRESTExercises/07.Students/tests/e2e.test.js
+++ JS Front-End/05.HTTPAndREST/01.HTTPAndRESTExercises/07.Students/tests/e2e.test.js
@@ -1,7 +1,7 @@
-const { chromium } = require('playwright-chromium');
-const { expect } = require('chai');
+const { chromium } = require("playwright-chromium");
+const { expect } = require("chai");
 
-const host = 'http://localhost:3000'; // Application host (NOT service host - that can be anything)
+const host = "http://localhost:3000"; // Application host (NOT service host - that can be anything)
 
 const DEBUG = false;
 const slowMo = 500;
@@ -9,31 +9,31 @@ const slowMo = 500;
 const mockData = {
   list: [
     {
-      firstName: 'Isaac',
-      lastName: 'Netero',
-      facultyNumber: '90000587896',
+      firstName: "Isaac",
+      lastName: "Netero",
+      facultyNumber: "90000587896",
       grade: 4.99,
-      _id: '1001',
+      _id: "1001",
     },
     {
-      firstName: 'Nvy',
-      lastName: 'Ose',
-      facultyNumber: '90000123456',
+      firstName: "Nvy",
+      lastName: "Ose",
+      facultyNumber: "90000123456",
       grade: 6,
-      _id: '1002',
+      _id: "1002",
     },
   ],
 };
 
 const endpoints = {
-  catalog: '/jsonstore/collections/students',
+  catalog: "/jsonstore/collections/students",
 };
 
 let browser;
 let context;
 let page;
 
-describe('E2E tests', function () {
+describe("E2E tests", function () {
   // Setup
   this.timeout(DEBUG ? 120000 : 7000);
   before(
@@ -54,14 +54,14 @@ describe('E2E tests', function () {
   });
 
   // Test proper
-  describe('Students', () => {
-    it('Load Students', async () => {
+  describe("Students", () => {
+    it("Load Students", async () => {
       const data = mockData.list;
       const { get } = await handle(endpoints.catalog);
       get(data);
 
       await page.goto(host);
-      await page.waitForSelector('tbody>tr');
+      await page.waitForSelector("tbody>tr");
 
       const students = await page.$$eval(`tbody tr`, (t) =>
         t.map((s) => s.textContent)
@@ -70,88 +70,88 @@ describe('E2E tests', function () {
       expect(students.length).to.equal(data.length);
     });
 
-    it('Check first name', async () => {
+    it("Check first name", async () => {
       const data = mockData.list[0];
       await page.goto(host);
 
       const { post } = await handle(endpoints.catalog);
       const { onRequest } = post();
 
-      await page.waitForSelector('.inputs');
+      await page.waitForSelector(".inputs");
 
-      await page.fill('input[name="firstName"]', data.firstName + '1');
-      await page.fill('input[name="lastName"]', data.lastName + '1');
-      await page.fill('input[name="facultyNumber"]', data.facultyNumber + '1');
-      await page.fill('input[name="grade"]', data.grade + '1');
+      await page.fill('input[name="firstName"]', data.firstName + "1");
+      await page.fill('input[name="lastName"]', data.lastName + "1");
+      await page.fill('input[name="facultyNumber"]', data.facultyNumber + "1");
+      await page.fill('input[name="grade"]', data.grade + "1");
 
-      const [request] = await Promise.all([onRequest(), page.click('#submit')]);
+      const [request] = await Promise.all([onRequest(), page.click("#submit")]);
 
       const postData = JSON.parse(request.postData());
 
-      expect(postData.firstName).to.equal(data.firstName + '1');
+      expect(postData.firstName).to.equal(data.firstName + "1");
     });
 
-    it('Check last name', async () => {
+    it("Check last name", async () => {
       const data = mockData.list[0];
       await page.goto(host);
 
       const { post } = await handle(endpoints.catalog);
       const { onRequest } = post();
 
-      await page.waitForSelector('.inputs');
+      await page.waitForSelector(".inputs");
 
-      await page.fill('input[name="firstName"]', data.firstName + '1');
-      await page.fill('input[name="lastName"]', data.lastName + '1');
-      await page.fill('input[name="facultyNumber"]', data.facultyNumber + '1');
-      await page.fill('input[name="grade"]', data.grade + '1');
+      await page.fill('input[name="firstName"]', data.firstName + "1");
+      await page.fill('input[name="lastName"]', data.lastName + "1");
+      await page.fill('input[name="facultyNumber"]', data.facultyNumber + "1");
+      await page.fill('input[name="grade"]', data.grade + "1");
 
-      const [request] = await Promise.all([onRequest(), page.click('#submit')]);
+      const [request] = await Promise.all([onRequest(), page.click("#submit")]);
 
       const postData = JSON.parse(request.postData());
 
-      expect(postData.lastName).to.equal(data.lastName + '1');
+      expect(postData.lastName).to.equal(data.lastName + "1");
     });
 
-    it('Check faculty number', async () => {
+    it("Check faculty number", async () => {
       const data = mockData.list[0];
       await page.goto(host);
 
       const { post } = await handle(endpoints.catalog);
       const { onRequest } = post();
 
-      await page.waitForSelector('.inputs');
+      await page.waitForSelector(".inputs");
 
-      await page.fill('input[name="firstName"]', data.firstName + '1');
-      await page.fill('input[name="lastName"]', data.lastName + '1');
-      await page.fill('input[name="facultyNumber"]', data.facultyNumber + '1');
-      await page.fill('input[name="grade"]', data.grade + '1');
+      await page.fill('input[name="firstName"]', data.firstName + "1");
+      await page.fill('input[name="lastName"]', data.lastName + "1");
+      await page.fill('input[name="facultyNumber"]', data.facultyNumber + "1");
+      await page.fill('input[name="grade"]', data.grade + "1");
 
-      const [request] = await Promise.all([onRequest(), page.click('#submit')]);
+      const [request] = await Promise.all([onRequest(), page.click("#submit")]);
 
       const postData = JSON.parse(request.postData());
 
-      expect(postData.facultyNumber).to.equal(data.facultyNumber + '1');
+      expect(postData.facultyNumber).to.equal(data.facultyNumber + "1");
     });
 
-    it('Check grade', async () => {
+    it("Check grade", async () => {
       const data = mockData.list[0];
       await page.goto(host);
 
       const { post } = await handle(endpoints.catalog);
       const { onRequest } = post();
 
-      await page.waitForSelector('.inputs');
+      await page.waitForSelector(".inputs");
 
-      await page.fill('input[name="firstName"]', data.firstName + '1');
-      await page.fill('input[name="lastName"]', data.lastName + '1');
-      await page.fill('input[name="facultyNumber"]', data.facultyNumber + '1');
-      await page.fill('input[name="grade"]', data.grade + '1');
+      await page.fill('input[name="firstName"]', data.firstName + "1");
+      await page.fill('input[name="lastName"]', data.lastName + "1");
+      await page.fill('input[name="facultyNumber"]', data.facultyNumber + "1");
+      await page.fill('input[name="grade"]', data.grade + "1");
 
-      const [request] = await Promise.all([onRequest(), page.click('#submit')]);
+      const [request] = await Promise.all([onRequest(), page.click("#submit")]);
 
       const postData = JSON.parse(request.postData());
 
-      expect(postData.grade).to.equal(data.grade + '1');
+      expect(postData.grade).to.equal(data.grade + "1");
     });
   });
 });
@@ -161,7 +161,7 @@ async function setupContext(context) {
   await handleContext(context, endpoints.catalog, { get: mockData.list });
   await handleContext(context, endpoints.catalog, { post: mockData.list[0] });
 
-  await handleContext(context, endpoints.delete('1001'), {
+  await handleContext(context, endpoints.delete("1001"), {
     get: mockData.list[0],
   });
 
@@ -170,7 +170,7 @@ async function setupContext(context) {
     (url) => url.href.slice(0, host.length) != host,
     (route) => {
       if (DEBUG) {
-        console.log('Preventing external call to ' + route.request().url());
+        console.log("Preventing external call to " + route.request().url());
       }
       route.abort();
     }
@@ -188,20 +188,20 @@ function handleContext(context, match, handlers) {
 async function handleRaw(match, handlers) {
   const methodHandlers = {};
   const result = {
-    get: (returns, options) => request('GET', returns, options),
-    get2: (returns, options) => request('GET', returns, options),
-    post: (returns, options) => request('POST', returns, options),
-    put: (returns, options) => request('PUT', returns, options),
-    patch: (returns, options) => request('PATCH', returns, options),
-    del: (returns, options) => request('DELETE', returns, options),
-    delete: (returns, options) => request('DELETE', returns, options),
+    get: (returns, options) => request("GET", returns, options),
+    get2: (returns, options) => request("GET", returns, options),
+    post: (returns, options) => request("POST", returns, options),
+    put: (returns, options) => request("PUT", returns, options),
+    patch: (returns, options) => request("PATCH", returns, options),
+    del: (returns, options) => request("DELETE", returns, options),
+    delete: (returns, options) => request("DELETE", returns, options),
   };
 
   const context = this;
 
   await context.route(urlPredicate, (route, request) => {
     if (DEBUG) {
-      console.log('>>>', request.method(), request.url());
+      console.log(">>>", request.method(), request.url());
     }
 
     const handler = methodHandlers[request.method().toLowerCase()];
@@ -214,7 +214,7 @@ async function handleRaw(match, handlers) {
 
   if (handlers) {
     for (let method in handlers) {
-      if (typeof handlers[method] == 'function') {
+      if (typeof handlers[method] == "function") {
         handlers[method](result[method]);
       } else {
         result[method](handlers[method]);
@@ -258,10 +258,10 @@ function respond(data, options = {}) {
   );
 
   const headers = {
-    'Access-Control-Allow-Origin': '*',
+    "Access-Control-Allow-Origin": "*",
   };
   if (options.json) {
-    headers['Content-Type'] = 'application/json';
+    headers["Content-Type"] = "application/json";
     data = JSON.stringify(data);
   }
 
