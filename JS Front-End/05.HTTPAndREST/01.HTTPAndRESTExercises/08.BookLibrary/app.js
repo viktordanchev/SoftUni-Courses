a06d2fc40481967b1c74a44b00716a369136b69a
VDanchev <viktordan315@gmail.com>
2023-07-28 21:21:18 +0300

    Added new solution

--- JS Front-End/05.HTTPAndREST/01.HTTPAndRESTExercises/08.BookLibrary/app.js
+++ JS Front-End/05.HTTPAndREST/01.HTTPAndRESTExercises/08.BookLibrary/app.js
@@ -1,5 +1,48 @@
 function attachEvents() {
-  // TODO:
+  const url = "http://localhost:3030/jsonstore/collections/books";
+  const tbody = document.querySelector("tbody");
+
+  document.getElementById("loadBooks").addEventListener("click", loadAllBooks);
+  document
+    .querySelector("#form button")
+    .addEventListener("click", createNewBook);
+
+  async function loadAllBooks() {
+    tbody.innerHTML = "";
+
+    const res = await (await fetch(url)).json();
+
+    for (const key in res) {
+      const tr = tbody.appendChild(document.createElement("tr"));
+
+      tr.appendChild(document.createElement("td")).textContent =
+        res[key]["author"];
+      tr.appendChild(document.createElement("td")).textContent =
+        res[key]["title"];
+      const tdButtons = tr.appendChild(document.createElement("td"));
+      tdButtons.appendChild(document.createElement("button")).textContent =
+        "Edit";
+      tdButtons.appendChild(document.createElement("button")).textContent =
+        "Delete";
+    }
+  }
+
+  async function createNewBook() {
+    const title = document.getElementsByName("title")[0];
+    const author = document.getElementsByName("author")[0];
+
+    if (!title || !author) {
+      return;
+    }
+
+    await fetch(url, {
+      method: "post",
+      body: JSON.stringify({ author: author.value, title: title.value }),
+    });
+
+    author.value = "";
+    title.value = "";
+  }
 }
 
 attachEvents();
