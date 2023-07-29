f5c48edc2f7f34e837d7726633e38d3131696101
VDanchev <viktordan315@gmail.com>
2023-07-29 20:05:21 +0300

    Added new solution

--- JS Front-End/05.HTTPAndREST/02.HTTPAndRESTMoreExercises/01.LockedProfile/app.js
+++ JS Front-End/05.HTTPAndREST/02.HTTPAndRESTMoreExercises/01.LockedProfile/app.js
@@ -1,3 +1,37 @@
 function lockedProfile() {
-  console.log("TODO...");
+  const url = "http://localhost:3030/jsonstore/advanced/profiles";
+  const main = document.getElementById("main");
+  main.innerHTML = "";
+
+  solve();
+
+  async function solve() {
+    let count = 1;
+    const res = await (await fetch(url)).json();
+
+    for (const key in res) {
+      const profileHtml = `<div class="profile">
+      <img src="./iconProfile2.png" class="userIcon" />
+      <label>Lock</label>
+      <input type="radio" name="user${count}Locked" value="lock" checked>
+      <label>Unlock</label>
+      <input type="radio" name="user${count}Locked" value="unlock"><br>
+      <hr>
+      <label>Username</label>
+      <input type="text" name="user${count}Username" value="${res[key]["username"]}" disabled readonly />
+      <div class="user${count}Username hiddenInfo">
+        <hr>
+        <label>Email:</label>
+        <input type="email" name="user${count}Email" value="${res[key]["email"]}" disabled readonly />
+        <label>Age:</label>
+        <input type="text" name="user${count}Age" value="${res[key]["age"]}" disabled readonly />
+      </div>
+
+      <button>Show more</button>
+    </div>`;
+
+      main.innerHTML += profileHtml;
+      count++;
+    }
+  }
 }
