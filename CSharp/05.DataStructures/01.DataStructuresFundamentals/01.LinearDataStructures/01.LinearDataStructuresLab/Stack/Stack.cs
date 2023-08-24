a84b1e975ca1114169ceeaec3514bb8a5c54ea54
VDanchev <viktordan315@gmail.com>
2023-08-24 17:27:41 +0300

    New folder and task added

---
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/01.LinearDataStructures/01.LinearDataStructuresLab/Stack/Stack.cs
@@ -0,0 +1,48 @@
+﻿namespace Problem02.Stack
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Stack<T> : IAbstractStack<T>
+    {
+        private class Node
+        {
+            
+        }
+
+        private Node top;
+
+        public int Count => throw new System.NotImplementedException();
+
+        public void Push(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Pop()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
