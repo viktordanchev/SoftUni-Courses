cb995a931873eb0840db3279638545a44bd9a636
VDanchev <viktordan315@gmail.com>
2023-08-24 17:27:39 +0300

    New folder and task added

---
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/01.LinearDataStructures/01.LinearDataStructuresLab/SinglyLinkedList/SinglyLinkedList.cs
@@ -0,0 +1,51 @@
+﻿namespace Problem04.SinglyLinkedList
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class SinglyLinkedList<T> : IAbstractLinkedList<T>
+    {
+        public int Count => throw new NotImplementedException();
+
+        public void AddFirst(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddLast(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetLast()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T RemoveFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T RemoveLast()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
