3879edfc3f5f780b61a595ac57d37a865a8d9029
VDanchev <viktordan315@gmail.com>
2023-08-24 17:27:32 +0300

    New folder and task added

---
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/01.LinearDataStructures/01.LinearDataStructuresLab/Queue/Queue.cs
@@ -0,0 +1,48 @@
+﻿namespace Problem03.Queue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Queue<T> : IAbstractQueue<T>
+    {
+        private class Node
+        {
+            
+        }
+
+        private Node head;
+
+        public int Count => throw new NotImplementedException();
+
+        public void Enqueue(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
