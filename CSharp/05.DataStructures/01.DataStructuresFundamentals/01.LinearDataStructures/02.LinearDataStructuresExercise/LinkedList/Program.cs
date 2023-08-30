1717147193fc447e5d0f3f612f69f14741d8e879
VDanchev <viktordan315@gmail.com>
2023-08-30 19:06:56 +0300

    Added new project

---
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/01.LinearDataStructures/02.LinearDataStructuresExercise/LinkedList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem02.DoublyLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
