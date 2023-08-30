16b63d1b29fbfe0cefaa1f476a0e37fd6a24a0f0
VDanchev <viktordan315@gmail.com>
2023-08-30 18:37:26 +0300

    Updated solution

---
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/01.LinearDataStructures/01.LinearDataStructuresLab/SingleLinkedList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem04.SinglyLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/01.LinearDataStructures/01.LinearDataStructuresLab/SingleLinkedList/SinglyLinkedList.cs
@@ -0,0 +1,130 @@
+﻿namespace Problem04.SinglyLinkedList
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class SinglyLinkedList<T> : IAbstractLinkedList<T>
+    {
+        private class Node
+        {
+            public Node(T element, Node next)
+            {
+                Element = element;
+                Next = next;
+            }
+
+            public T Element { get; set; }
+            public Node Next { get; set; }
+        }
+
+        private Node node;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(T item)
+        {
+            node = new Node(item, node);
+            Count++;
+        }
+
+        public void AddLast(T item)
+        {
+            if(Count == 0)
+            {
+                node = new Node(item, node);
+            }
+            else
+            {
+                Node currNode = node;
+
+                while (currNode.Next != null)
+                {
+                    currNode = currNode.Next;
+                }
+
+                currNode.Next = new Node(item, null);
+            }
+
+            Count++;
+        }
+
+        public T GetFirst()
+        {
+            IsEmpty();
+            return node.Element;
+        }
+
+        public T GetLast()
+        {
+            IsEmpty();
+            return GetFirstElement(Count);
+        }
+
+        public T RemoveFirst()
+        {
+            IsEmpty();
+
+            T currElement = node.Element;
+            node = node.Next;
+            Count--;
+            return currElement;
+        }
+
+        public T RemoveLast()
+        {
+            IsEmpty();
+
+            T element = GetFirstElement(Count);
+            Node currNode = node;
+
+            Count--;
+            for (int i = 1; i <= Count; i++)
+            {
+                if (i == Count)
+                {
+                    currNode.Next = null;
+                    break;
+                }
+
+                currNode = currNode.Next;
+            }
+
+            return element;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node currNode = node;
+
+            for (int i = 0; i < Count; i++)
+            {
+                yield return currNode.Element;
+                currNode = currNode.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private void IsEmpty()
+        {
+            if (node == null)
+                throw new InvalidOperationException();
+        }
+
+        private T GetFirstElement(int position)
+        {
+            Node currNode = node;
+
+            for (int i = 0; i < position - 1; i++)
+            {
+                currNode = currNode.Next;
+            }
+
+            return currNode.Element;
+        }
+    }
+}
