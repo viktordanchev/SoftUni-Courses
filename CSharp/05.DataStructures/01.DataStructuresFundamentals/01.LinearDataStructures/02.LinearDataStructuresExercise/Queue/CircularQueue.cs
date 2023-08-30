8bbdd60b47d49c866509d6a0521467f879b2421e
VDanchev <viktordan315@gmail.com>
2023-08-30 19:06:30 +0300

    Added new project

---
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/01.LinearDataStructures/02.LinearDataStructuresExercise/Queue/CircularQueue.cs
@@ -0,0 +1,42 @@
+﻿namespace Problem01.CircularQueue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class CircularQueue<T> : IAbstractQueue<T>
+    {
+        public int Count => throw new NotImplementedException();
+
+        public T Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Enqueue(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T[] ToArray()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/01.LinearDataStructures/02.LinearDataStructuresExercise/Queue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem01.CircularQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
