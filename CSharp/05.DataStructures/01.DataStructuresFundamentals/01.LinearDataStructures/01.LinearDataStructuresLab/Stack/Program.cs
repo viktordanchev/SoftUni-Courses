e64a9e17a640185b23d025932ba815dd82e7df5c
VDanchev <viktordan315@gmail.com>
2023-08-30 18:37:28 +0300

    Updated solution

---
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/01.LinearDataStructures/01.LinearDataStructuresLab/Stack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem02.Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
