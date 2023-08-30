1eb43adfaefa74f68c12dff6995b4f1103677a30
VDanchev <viktordan315@gmail.com>
2023-08-30 19:06:48 +0300

    Added new project

---
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/01.LinearDataStructures/02.LinearDataStructuresExercise/List/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem03.ReversedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
