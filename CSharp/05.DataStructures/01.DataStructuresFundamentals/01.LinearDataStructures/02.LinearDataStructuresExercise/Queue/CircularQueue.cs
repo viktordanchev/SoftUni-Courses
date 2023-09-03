4a60d2b381a600fff8db4d459c36183bb95ae608
VDanchev <viktordan315@gmail.com>
2023-09-03 16:00:22 +0300

    Added new solution

--- CSharp/05.DataStructures/01.DataStructuresFundamentals/01.LinearDataStructures/02.LinearDataStructuresExercise/Queue/CircularQueue.cs
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/01.LinearDataStructures/02.LinearDataStructuresExercise/Queue/CircularQueue.cs
@@ -6,37 +6,91 @@
 
     public class CircularQueue<T> : IAbstractQueue<T>
     {
-        public int Count { get; private set; }
+        private const int DefaultCapacity = 4;
+        private T[] items;
+        private int firstItemIndex;
+        private int lastItemIndex;
 
-        public T Dequeue()
+        public CircularQueue()
+            : this(DefaultCapacity)
         {
-            throw new NotImplementedException();
         }
+
+        public CircularQueue(int capacity)
+        {
+            items = new T[capacity];
+        }
+
+        public int Count { get; private set; }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            if (Count == items.Length)
+            {
+                T[] newArray = new T[Count * 2];
+                Array.Copy(items, newArray, Count);
+                items = newArray;
+                firstItemIndex = 0;
+                lastItemIndex = Count;
+            }
+
+            items[lastItemIndex] = item;
+            lastItemIndex = (lastItemIndex + 1) % items.Length;
+            Count++;
+        }
+
+        public T Dequeue()
+        {
+            IsEmpty();
+
+            T currItem = items[firstItemIndex];
+
+            items[firstItemIndex] = default;
+            firstItemIndex = (firstItemIndex + 1) % items.Length;
+            Count--;
+
+            return currItem;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            IsEmpty();
+            return items[firstItemIndex];
         }
 
         public T[] ToArray()
         {
-            throw new NotImplementedException();
+            T[] result = new T[Count];
+
+            for (int i = 0; i < Count; i++)
+            {
+                int index = (firstItemIndex + i) % items.Length;
+                result[i] = items[index];
+            }
+
+            return result;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < Count; i++)
+            {
+                int index = (firstItemIndex + i) % items.Length;
+                yield return items[index];
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
         }
-    }
 
+        private void IsEmpty()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
 }
