1c2a058f503e86e02fedea8e6e8537ebb93c42c5
VDanchev <viktordan315@gmail.com>
2023-09-04 22:57:40 +0300

    Added new solution

--- CSharp/05.DataStructures/01.DataStructuresFundamentals/02.TreesRepresentation/01.TreesRepresentationLab/Tree/Program.cs
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/02.TreesRepresentation/01.TreesRepresentationLab/Tree/Program.cs
@@ -1,15 +1,21 @@
-﻿namespace Tree
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
+﻿using System;
 
+namespace Tree
+{
     public class Program
     {
         public static void Main(string[] args)
         {
+            var tree = new Tree<int>(1, 
+                           new Tree<int>(2),
+                           new Tree<int>(3),
+                           new Tree<int>(4, 
+                               new Tree<int>(5))
+                           );
 
-            
+            var tree2 = new Tree<int>(1);
+
+            Console.WriteLine(string.Join(", ", tree.OrderDfs()));
         }
     }
 }
