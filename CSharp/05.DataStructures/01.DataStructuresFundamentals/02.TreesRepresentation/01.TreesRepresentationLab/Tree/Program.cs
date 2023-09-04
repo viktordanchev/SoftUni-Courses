3695bd7c2ef6cd054272c946e288829a6076a1cd
VDanchev <viktordan315@gmail.com>
2023-09-04 17:34:25 +0300

    Added new project

---
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/02.TreesRepresentation/01.TreesRepresentationLab/Tree/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            
+        }
+    }
+}
---
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/02.TreesRepresentation/01.TreesRepresentationLab/Tree/Tree.cs
@@ -0,0 +1,42 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Tree<T> : IAbstractTree<T>
+    {
+        public Tree(T value)
+        {
+        }
+
+        public Tree(T value, params Tree<T>[] children)
+            : this(value)
+        {
+        }
+
+        public void AddChild(T parentKey, Tree<T> child)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> OrderBfs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> OrderDfs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveNode(T nodeKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Swap(T firstKey, T secondKey)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
