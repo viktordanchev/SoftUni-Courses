36b88e798f3de631fd0975e5cb6e43f6e4ae1326
VDanchev <viktordan315@gmail.com>
2023-09-04 22:57:43 +0300

    Added new solution

--- CSharp/05.DataStructures/01.DataStructuresFundamentals/02.TreesRepresentation/01.TreesRepresentationLab/Tree/Tree.cs
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/02.TreesRepresentation/01.TreesRepresentationLab/Tree/Tree.cs
@@ -5,13 +5,24 @@
 
     public class Tree<T> : IAbstractTree<T>
     {
+        private T value;
+        private Tree<T> parent;
+        private List<Tree<T>> children;
+
         public Tree(T value)
         {
+            this.value = value;
+            children = new List<Tree<T>>();
         }
 
         public Tree(T value, params Tree<T>[] children)
             : this(value)
         {
+            foreach (var child in children)
+            {
+                child.parent = this;
+                this.children.Add(child);
+            }
         }
 
         public void AddChild(T parentKey, Tree<T> child)
@@ -21,12 +32,43 @@
 
         public IEnumerable<T> OrderBfs()
         {
-            throw new NotImplementedException();
+            var result = new List<T>();
+            var items = new Queue<Tree<T>>();
+
+            items.Enqueue(this);
+            while (items.Count > 0)
+            {
+                var currItem = items.Dequeue();
+                result.Add(currItem.value);
+
+                foreach (var child in currItem.children)
+                {
+                    items.Enqueue(child);
+                }
+            }
+
+            return result;
         }
 
         public IEnumerable<T> OrderDfs()
         {
-            throw new NotImplementedException();
+            var result = new Stack<T>();
+            var items = new Stack<Tree<T>>();
+
+            items.Push(this);
+            while (items.Count > 0)
+            {
+                var currItem = items.Pop();
+
+                foreach (var child in currItem.children)
+                {
+                    items.Push(child);
+                }
+
+                result.Push(currItem.value);
+            }
+
+            return result;
         }
 
         public void RemoveNode(T nodeKey)
