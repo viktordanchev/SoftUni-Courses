4754be998327d63e62d8b2475c9b6b499e2a3975
VDanchev <viktordan315@gmail.com>
2023-09-10 23:34:47 +0300

    Added new solution

---
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/01.LinearDataStructures/02.LinearDataStructuresExercise/CalcSequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace CalcSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue();
+            var number = int.Parse(Console.ReadLine());
+
+            queue.Enqueue(number);
+            for (int i = 1; i < 17; i++)
+            {
+                var currNumber = number;
+                currNumber = currNumber + 1;
+                queue.Enqueue(currNumber);
+                currNumber = number;
+                currNumber = currNumber * 2 + 1;
+                queue.Enqueue(currNumber);
+                currNumber = number;
+                currNumber = currNumber + 2;
+                queue.Enqueue(currNumber);
+            }
+
+            foreach (var item in queue)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
