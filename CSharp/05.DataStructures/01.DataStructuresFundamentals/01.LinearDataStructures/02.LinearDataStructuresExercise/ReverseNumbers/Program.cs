8133b418e12800264ade44a4a784a7932b04dd1b
VDanchev <viktordan315@gmail.com>
2023-09-10 23:34:49 +0300

    Added new solution

---
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/01.LinearDataStructures/02.LinearDataStructuresExercise/ReverseNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<string>();
+
+            var input = Console.ReadLine().Split(" ").ToArray();
+
+            foreach (var item in input)
+            {
+                stack.Push(item);
+            }
+
+            Console.WriteLine(string.Join(" ", stack));
+        }
+    }
+}
