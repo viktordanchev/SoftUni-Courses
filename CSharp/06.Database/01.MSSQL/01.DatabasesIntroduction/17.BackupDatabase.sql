5bfd1667a1112c249d60af1fb3930385758b35fe
VDanchev <viktordan315@gmail.com>
2023-09-19 16:37:12 +0300

    Added new solution

---
+++ CSharp/06.Database/01.MSSQL/01.DatabasesIntroduction/17.BackupDatabase.sql
@@ -0,0 +1,5 @@
+BACKUP DATABASE [SoftUni] 
+	TO DISK = 'C:\Users\vikto\Desktop\Database Backup\softuni-backup.bak'
+
+RESTORE DATABASE [SoftUni]
+	FROM DISK = 'C:\Users\vikto\Desktop\Database Backup\softuni-backup.bak'
