4f236f3dc6086609a78d660d14f62d38fa388055
VDanchev <viktordan315@gmail.com>
2023-09-25 20:19:11 +0300

    Added new solution

---
+++ MSSQL/04.Built-inFunctions/13.MixOfPeakAndRiverNames.sql
@@ -0,0 +1,9 @@
+SELECT [PeakName], [RiverName], LOWER(
+	CONCAT([PeakName], 
+	SUBSTRING([RiverName], 2, 
+	LEN([RiverName])))) AS [Mix]
+FROM [Peaks], [Rivers] 
+WHERE SUBSTRING([PeakName], LEN([PeakName]), 1) = LEFT([RiverName], 1)
+ORDER BY [Mix]
+
+
