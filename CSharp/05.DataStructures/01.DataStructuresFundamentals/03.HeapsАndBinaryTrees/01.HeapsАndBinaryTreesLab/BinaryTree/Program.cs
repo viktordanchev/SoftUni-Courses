cd62d14f07ddc693c3341bd108b5a3fb8af1a0d6
VDanchev <viktordan315@gmail.com>
2023-09-26 16:04:28 +0300

    Added new project

---
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/03.HeapsАndBinaryTrees/01.HeapsАndBinaryTreesLab/BinaryTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
