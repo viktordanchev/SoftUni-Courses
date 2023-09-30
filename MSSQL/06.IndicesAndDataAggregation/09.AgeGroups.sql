98487e4028c59a73957aeb4a8c9db763769f3b23
Viktor Danchev <viktordan315@gmail.com>
2023-09-30 18:23:30 +0300

    Added new solution

---
+++ MSSQL/06.IndicesAndDataAggregation/09.AgeGroups.sql
@@ -0,0 +1,17 @@
+SELECT t.[AgeGroup]
+	,SUM(t.[Count]) AS [WizardCount]
+FROM (
+SELECT 
+	CASE 
+		WHEN [Age] BETWEEN 0 AND 10 THEN '[0-10]'
+		WHEN [Age] BETWEEN 11 AND 20 THEN '[11-20]'
+		WHEN [Age] BETWEEN 21 AND 30 THEN '[21-30]'
+		WHEN [Age] BETWEEN 31 AND 40 THEN '[31-40]'
+		WHEN [Age] BETWEEN 41 AND 50 THEN '[41-50]'
+		WHEN [Age] BETWEEN 51 AND 60 THEN '[51-60]'
+		ELSE '[61+]'
+	END AS [AgeGroup]
+	,COUNT(*) AS [Count]
+FROM [WizzardDeposits]
+GROUP BY [Age]) AS t
+GROUP BY t.[AgeGroup]
