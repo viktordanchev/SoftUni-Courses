fc4db5c96a1b34a9b53e87cc64ec1c0a8550711f
Viktor Danchev <viktordan315@gmail.com>
2023-09-30 18:23:55 +0300

    Added new solution

---
+++ MSSQL/06.IndicesAndDataAggregation/14.EmployeesMinimumSalaries.sql
@@ -0,0 +1,6 @@
+SELECT [DepartmentId], MIN([Salary]) AS [MinimumSalary]
+FROM [Employees]
+WHERE [DepartmentID] IN (2, 5, 7) 
+	AND [HireDate] > '2000-01-01'
+GROUP BY [DepartmentID]
+ORDER BY [DepartmentID]
