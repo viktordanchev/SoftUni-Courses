2faa7fb525ebc1493c03275a65041ad313d39a20
VDanchev <viktordan315@gmail.com>
2023-10-03 17:32:36 +0300

    Update

--- CSharp/05.DataStructures/01.DataStructuresFundamentals/03.HeapsАndBinaryTrees/01.HeapsАndBinaryTreesLab/BinaryTree/Program.cs
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/03.HeapsАndBinaryTrees/01.HeapsАndBinaryTreesLab/BinaryTree/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _01.BinaryTree;
+using System;
 
 namespace BinaryTree
 {
@@ -6,7 +7,20 @@ namespace BinaryTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var tree = new BinaryTree<int>(
+                        1,
+                        new BinaryTree<int>(
+                            2,
+                            new BinaryTree<int>(4, null, null),
+                            new BinaryTree<int>(5, null, null)
+                        ),
+                        new BinaryTree<int>(
+                            3,
+                            new BinaryTree<int>(6, null, null),
+                            new BinaryTree<int>(7, null, null)
+                        ));
+
+            Console.WriteLine(tree.AsIndentedPreOrder(0));
         }
     }
 }
