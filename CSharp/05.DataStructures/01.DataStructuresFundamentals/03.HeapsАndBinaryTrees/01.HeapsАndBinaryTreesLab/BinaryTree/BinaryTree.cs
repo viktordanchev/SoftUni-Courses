c6e51b32838a2329090d49eb3b76ac317a5052ec
VDanchev <viktordan315@gmail.com>
2023-10-03 17:32:29 +0300

    Added solution

--- CSharp/05.DataStructures/01.DataStructuresFundamentals/03.HeapsАndBinaryTrees/01.HeapsАndBinaryTreesLab/BinaryTree/BinaryTree.cs
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/03.HeapsАndBinaryTrees/01.HeapsАndBinaryTreesLab/BinaryTree/BinaryTree.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Text;
 
     public class BinaryTree<T> : IAbstractBinaryTree<T>
     {
@@ -20,7 +21,31 @@
 
         public string AsIndentedPreOrder(int indent)
         {
-            throw new NotImplementedException();
+            var result = new StringBuilder();
+            var tree = new Stack<IAbstractBinaryTree<T>>();
+
+            tree.Push(this);
+
+            while (tree.Count > 0)
+            {
+                var node = tree.Pop();
+
+                result.AppendLine($"{Repeat(indent, " ")}{node.Value}");
+
+                if (node.RightChild != null)
+                {
+                    tree.Push(node.RightChild);
+                }
+
+                if (node.LeftChild != null)
+                {
+                    tree.Push(node.LeftChild);
+                }
+
+                indent += 2;
+            }
+
+            return result.ToString();
         }
 
         public void ForEachInOrder(Action<T> action)
@@ -30,27 +55,91 @@
 
         public IEnumerable<IAbstractBinaryTree<T>> InOrder()
         {
-            throw new NotImplementedException();
+            var result = new List<IAbstractBinaryTree<T>>();
+            var tree = new Stack<IAbstractBinaryTree<T>>();
+
+            tree.Push(this);
+
+            while (tree.Count > 0)
+            {
+                var node = tree.Peek();
+
+                if (node.LeftChild != null)
+                {
+                    tree.Push(node.LeftChild);
+                    continue;
+                }
+
+                result.Add(tree.Pop());
+
+                if (tree.Count == 0)
+                    break;
+
+                node = tree.Pop();
+                result.Add(node);
+
+                if (node.RightChild != null)
+                    tree.Push(node.RightChild);
+            }
+
+            return result;
         }
 
         public IEnumerable<IAbstractBinaryTree<T>> PostOrder()
         {
-            throw new NotImplementedException();
+            var result = new Stack<IAbstractBinaryTree<T>>();
+            var tree = new Stack<IAbstractBinaryTree<T>>();
+
+            tree.Push(this);
+
+            while (tree.Count > 0)
+            {
+                var node = tree.Pop();
+                result.Push(node);
+
+                if (node.LeftChild != null)
+                    tree.Push(node.LeftChild);
+
+                if (node.RightChild != null)
+                    tree.Push(node.RightChild);
+            }
+
+            return result;
         }
 
         public IEnumerable<IAbstractBinaryTree<T>> PreOrder()
         {
             var result = new List<IAbstractBinaryTree<T>>();
+            var tree = new Stack<IAbstractBinaryTree<T>>();
 
-            result.Add(this);
+            tree.Push(this);
 
-            if (this.LeftChild != null)
-                result.AddRange(this.LeftChild.PreOrder());
+            while (tree.Count > 0)
+            {
+                var node = tree.Pop();
 
-            if (this.RightChild != null)
-                result.AddRange(this.RightChild.PreOrder());
+                result.Add(node);
+
+                if (node.RightChild != null)
+                    tree.Push(node.RightChild);
+
+                if (node.LeftChild != null)
+                    tree.Push(node.LeftChild);
+            }
 
             return result;
         }
-}
+
+        private string Repeat(int count, string str)
+        {
+            var output = new StringBuilder();
+
+            for (int i = 0; i < count; i++)
+            {
+                output.Append(str);
+            }
+
+            return output.ToString();
+        }
+    }
 }
