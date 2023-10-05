3f80569519aeb149aa7d2feb3bd9a057900e7c73
VDanchev <viktordan315@gmail.com>
2023-10-05 14:29:14 +0300

    New project added

---
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/ExamPreparation/RetakeExam18December2022/ViTube/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Exam.ViTube
+{
+    public class User
+    {
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+
+        public User(string id, string username)
+        {
+            Id = id;
+            Username = username;
+        }
+    }
+}
---
+++ CSharp/05.DataStructures/01.DataStructuresFundamentals/ExamPreparation/RetakeExam18December2022/ViTube/ViTubeRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exam.ViTube
+{
+    public class ViTubeRepository : IViTubeRepository
+    {
+        public bool Contains(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(Video video)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DislikeVideo(User user, Video video)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetPassiveUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetUsersByActivityThenByName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Video> GetVideos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Video> GetVideosOrderedByViewsThenByLikesThenByDislikes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LikeVideo(User user, Video video)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PostVideo(Video video)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WatchVideo(User user, Video video)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
