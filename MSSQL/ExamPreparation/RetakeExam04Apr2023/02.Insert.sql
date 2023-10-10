ddf17ba5d79415437d9ee810353e043defc919fb
VDanchev <viktordan315@gmail.com>
2023-10-10 18:09:43 +0300

    Added new solution

---
+++ MSSQL/ExamPreparation/RetakeExam04Apr2023/02.Insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO [Products]
+	VALUES
+	('SCANIA Oil Filter XD01', 78.69, 1, 1),
+	('MAN Air Filter XD01', 97.38, 1, 5),
+	('DAF Light Bulb 05FG87', 55.00, 2, 13),
+	('ADR Shoes 47-47.5', 49.85, 3, 5),
+	('Anti-slip pads S', 5.87, 5, 7)
+
+INSERT INTO [Invoices]
+	VALUES
+	(1219992181, '2023-03-01', '2023-04-30', 180.96, 'BGN', 3),
+	(1729252340, '2022-11-06', '2023-01-04', 158.18, 'EUR', 13),
+	(1950101013, '2023-02-17', '2023-04-18', 615.15, 'USD', 19)
