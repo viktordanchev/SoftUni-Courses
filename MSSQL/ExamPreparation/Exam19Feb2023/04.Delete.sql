98d5558c399925e8baef0636516bef33e9aabdca
VDanchev <viktordan315@gmail.com>
2023-10-11 17:57:51 +0300

    Added new solution

---
+++ MSSQL/ExamPreparation/Exam19Feb2023/04.Delete.sql
@@ -0,0 +1,15 @@
+DELETE 
+FROM [CreatorsBoardgames]
+WHERE [BoardgameId] IN (1, 16, 31, 47)
+
+DELETE 
+FROM [Boardgames]
+WHERE [PublisherId] IN (1, 16)
+
+DELETE 
+FROM [Publishers]
+WHERE [AddressId] = 5
+
+DELETE 
+FROM [Addresses]
+WHERE LEFT([Town], 1) = 'L'  
