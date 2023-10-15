26b97a5626d97d01255bba699d0ec14ffea347f4
VDanchev <viktordan315@gmail.com>
2023-10-15 10:56:27 +0300

    Added new solution

---
+++ MSSQL/FinalExam/02.Insert.sql
@@ -0,0 +1,16 @@
+﻿INSERT INTO [Tourists]
+	VALUES
+	('John Rivers', '653-551-1555', 'john.rivers@example.com', 6),
+	('Adeline Aglaé', '122-654-8726', 'adeline.aglae@example.com', 2),
+	('Sergio Ramirez', '233-465-2876', 's.ramirez@example.com', 3),
+	('Johan Müller', '322-876-9826', 'j.muller@example.com', 7),
+	('Eden Smith', '551-874-2234', 'eden.smith@example.com', 6)
+
+INSERT INTO [Bookings]
+	VALUES
+	('2024-03-01', '2024-03-11', 1, 0, 21, 3, 5),
+	('2023-12-28', '2024-01-06', 2, 1, 22, 13, 3),
+	('2023-11-15', '2023-11-20', 1, 2, 23, 19, 7),
+	('2023-12-05', '2023-12-09', 4, 0, 24, 6, 4),
+	('2024-05-01', '2024-05-07', 6, 0, 25, 14, 6)
+
