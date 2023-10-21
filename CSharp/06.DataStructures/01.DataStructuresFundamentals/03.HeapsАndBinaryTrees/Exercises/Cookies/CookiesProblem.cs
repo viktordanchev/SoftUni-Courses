07b0394b0c66df4ec7930c4ee0df793ee4d434cb
VDanchev <viktordan315@gmail.com>
2023-10-21 19:08:10 +0300

    Added new project

---
+++ CSharp/06.DataStructures/01.DataStructuresFundamentals/03.HeapsАndBinaryTrees/Exercises/Cookies/CookiesProblem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04.CookiesProblem
+{
+    public class CookiesProblem
+    {
+        public int Solve(int minSweetness, int[] cookies)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
