75515f959c3d9316d2925504e1eab827cf162165
VDanchev <viktordan315@gmail.com>
2023-10-21 19:08:03 +0300

    Added new project

---
+++ CSharp/06.DataStructures/01.DataStructuresFundamentals/03.HeapsАndBinaryTrees/Exercises/BSTOperations/BinarySearchTree.cs
@@ -0,0 +1,194 @@
+﻿namespace _02.BinarySearchTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+
+    public class BinarySearchTree<T> : IBinarySearchTree<T> where T : IComparable
+    {
+        private class Node
+        {
+            public Node(T value)
+            {
+                this.Value = value;
+            }
+
+            public T Value { get; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+        }
+
+        private Node root;
+
+        private BinarySearchTree(Node node)
+        {
+            this.PreOrderCopy(node);
+        }
+
+        public BinarySearchTree()
+        {
+        }
+
+        public void Insert(T element)
+        {
+            this.root = this.Insert(element, this.root);
+        }
+
+        public bool Contains(T element)
+        {
+            Node current = this.FindElement(element);
+
+            return current != null;
+        }
+
+        public void EachInOrder(Action<T> action)
+        {
+            this.EachInOrder(this.root, action);
+        }
+
+        public IBinarySearchTree<T> Search(T element)
+        {
+            Node current = this.FindElement(element);
+
+            return new BinarySearchTree<T>(current);
+        }
+
+        public void Delete(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteMax()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteMin()
+        {
+            Node currRoot = root;
+
+            while(root.Left != null)
+            {
+                root = root.Left;
+            }
+
+            root = null;
+            root = currRoot;
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Rank(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Select(int rank)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Ceiling(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Floor(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Range(T startRange, T endRange)
+        {
+            var result = new List<T>();
+            var start = new Stack<T>();
+            
+            var currRoot = root;
+
+            while (!currRoot.Value.Equals(startRange))
+            {
+                start.Push(currRoot.Value);
+
+                if (currRoot.Value.CompareTo(startRange) > 0)
+                {
+                    currRoot = currRoot.Left;
+                }
+                else
+                {
+                    currRoot = currRoot.Right;
+                }
+            }
+
+            result.AddRange(start.ToArray());
+            return result;
+        }
+
+        private Node FindElement(T element)
+        {
+            Node current = this.root;
+
+            while (current != null)
+            {
+                if (current.Value.CompareTo(element) > 0)
+                {
+                    current = current.Left;
+                }
+                else if (current.Value.CompareTo(element) < 0)
+                {
+                    current = current.Right;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return current;
+        }
+
+        private void PreOrderCopy(Node node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            this.Insert(node.Value);
+            this.PreOrderCopy(node.Left);
+            this.PreOrderCopy(node.Right);
+        }
+
+        private Node Insert(T element, Node node)
+        {
+            if (node == null)
+            {
+                node = new Node(element);
+            }
+            else if (element.CompareTo(node.Value) < 0)
+            {
+                node.Left = this.Insert(element, node.Left);
+            }
+            else if (element.CompareTo(node.Value) > 0)
+            {
+                node.Right = this.Insert(element, node.Right);
+            }
+
+            return node;
+        }
+
+        private void EachInOrder(Node node, Action<T> action)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            this.EachInOrder(node.Left, action);
+            action(node.Value);
+            this.EachInOrder(node.Right, action);
+        }
+    }
+}
---
+++ CSharp/06.DataStructures/01.DataStructuresFundamentals/03.HeapsАndBinaryTrees/Exercises/BSTOperations/Program.cs
@@ -0,0 +1,29 @@
+﻿using _02.BinarySearchTree;
+using System;
+using System.Collections.Generic;
+
+namespace BSTOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree<int> bst = new BinarySearchTree<int>();
+
+            bst.Insert(10);
+            bst.Insert(5);
+            bst.Insert(3);
+            bst.Insert(1);
+            bst.Insert(4);
+            bst.Insert(8);
+            bst.Insert(9);
+            bst.Insert(37);
+            bst.Insert(39);
+            bst.Insert(45);
+
+            bst.DeleteMin();
+            //4, 5, 8, 9, 10, 37
+            Console.WriteLine(string.Join(", ", bst.Range(4, 37)));
+        }
+    }
+}
