acb84c31c65c47394ebd3030c0d768e68c7ed271
VDanchev <viktordan315@gmail.com>
2023-10-21 19:08:41 +0300

    Added new project

---
+++ CSharp/06.DataStructures/01.DataStructuresFundamentals/03.HeapsАndBinaryTrees/Exercises/MinHeap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MinHeap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
