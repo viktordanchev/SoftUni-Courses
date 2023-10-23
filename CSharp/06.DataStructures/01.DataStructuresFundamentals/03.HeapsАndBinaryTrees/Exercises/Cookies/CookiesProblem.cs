6edf9c60b9cb03ed8875bb3f805dafeb1b5e46fd
VDanchev <viktordan315@gmail.com>
2023-10-23 22:18:26 +0300

    Added new solution

--- CSharp/06.DataStructures/01.DataStructuresFundamentals/03.HeapsАndBinaryTrees/Exercises/Cookies/CookiesProblem.cs
+++ CSharp/06.DataStructures/01.DataStructuresFundamentals/03.HeapsАndBinaryTrees/Exercises/Cookies/CookiesProblem.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace _04.CookiesProblem
 {
@@ -6,7 +6,47 @@ namespace _04.CookiesProblem
     {
         public int Solve(int minSweetness, int[] cookies)
         {
-            throw new NotImplementedException();
+            var list = new List<int>(cookies);
+            list.Sort();
+
+            var count = 0;
+            while (list.Count > 0)
+            {
+                var cookie = list[0];
+
+                if(AreAllGreater(list, minSweetness))
+                {
+                    return count;
+                }
+
+                if (cookie < minSweetness)
+                {
+                    if(list.Count > 1)
+                    {
+                        cookie += 2 * list[1];
+                        list[1] = cookie;
+                    }
+
+                    count++;
+                }
+
+                list.RemoveAt(0);
+            }
+
+            return -1;
+        }
+
+        private bool AreAllGreater(List<int> cookies, int minSweetness) 
+        {
+            for (int i = 0; i < cookies.Count; i++)
+            {
+                if (cookies[i] < minSweetness)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
--- CSharp/06.DataStructures/01.DataStructuresFundamentals/03.HeapsАndBinaryTrees/Exercises/Cookies/Program.cs
+++ CSharp/06.DataStructures/01.DataStructuresFundamentals/03.HeapsАndBinaryTrees/Exercises/Cookies/Program.cs
@@ -6,7 +6,8 @@ namespace _04.CookiesProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var n = new CookiesProblem();
+            Console.WriteLine(n.Solve(10, new int[] { 1, 1, 1, 1 }));
         }
     }
 }
