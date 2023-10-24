b3f7cb8adbe6a2440b2413447b72ebc5c0085443
VDanchev <viktordan315@gmail.com>
2023-10-24 17:03:54 +0300

    Update

--- CSharp/06.DataStructures/01.DataStructuresFundamentals/03.HeapsАndBinaryTrees/Exercises/MinHeap/Program.cs
+++ CSharp/06.DataStructures/01.DataStructuresFundamentals/03.HeapsАndBinaryTrees/Exercises/MinHeap/Program.cs
@@ -1,5 +1,4 @@
-﻿using _03.MinHeap;
-using System;
+﻿using System;
 
 namespace MinHeap
 {
@@ -7,15 +6,7 @@ namespace MinHeap
     {
         static void Main(string[] args)
         {
-            var c = new PriorityQueue<int>();
-            c.Add(4);
-            c.Add(2);
-            c.Add(1);
-            c.Add(3);
-            Console.WriteLine(c.ExtractMin());
-            Console.WriteLine(c.ExtractMin());
-            Console.WriteLine(c.ExtractMin());
-            Console.WriteLine(c.ExtractMin());
+            Console.WriteLine("Hello World!");
         }
     }
 }
