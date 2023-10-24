4ee7aab116f9031687ee96b10cbeb67cfb558649
VDanchev <viktordan315@gmail.com>
2023-10-24 17:03:56 +0300

    Update

--- CSharp/06.DataStructures/01.DataStructuresFundamentals/03.HeapsАndBinaryTrees/Exercises/TopView/Program.cs
+++ CSharp/06.DataStructures/01.DataStructuresFundamentals/03.HeapsАndBinaryTrees/Exercises/TopView/Program.cs
@@ -1,5 +1,4 @@
-﻿using _05.TopView;
-using System;
+﻿using System;
 
 namespace TopView
 {
@@ -7,16 +6,7 @@ namespace TopView
     {
         static void Main(string[] args)
         {
-            var binaryTree =
-            new BinaryTree<int>(1,
-                    new BinaryTree<int>(2,
-                            new BinaryTree<int>(4, null, null),
-                            new BinaryTree<int>(5, null, null)),
-                    new BinaryTree<int>(3,
-                            new BinaryTree<int>(6, null, null),
-                            new BinaryTree<int>(7, null, null)));
-
-            Console.WriteLine(string.Join(", ", binaryTree.TopView()));
+            Console.WriteLine("Hello World!");
         }
     }
 }
