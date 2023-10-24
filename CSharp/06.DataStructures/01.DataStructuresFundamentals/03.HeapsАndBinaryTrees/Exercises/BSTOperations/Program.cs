b26510a296a6030c4381ea60245599e5729a2b21
VDanchev <viktordan315@gmail.com>
2023-10-24 17:03:51 +0300

    Update

--- CSharp/06.DataStructures/01.DataStructuresFundamentals/03.HeapsАndBinaryTrees/Exercises/BSTOperations/Program.cs
+++ CSharp/06.DataStructures/01.DataStructuresFundamentals/03.HeapsАndBinaryTrees/Exercises/BSTOperations/Program.cs
@@ -1,5 +1,4 @@
-﻿using _02.BinarySearchTree;
-using System;
+﻿using System;
 
 namespace BSTOperations
 {
@@ -7,20 +6,7 @@ namespace BSTOperations
     {
         static void Main(string[] args)
         {
-            BinarySearchTree<int> bst = new BinarySearchTree<int>();
-
-            bst.Insert(10);
-            bst.Insert(5);
-            bst.Insert(3);
-            bst.Insert(1);
-            bst.Insert(4);
-            bst.Insert(8);
-            bst.Insert(9);
-            bst.Insert(37);
-            bst.Insert(39);
-            bst.Insert(45);
-
-            Console.WriteLine(bst.Floor(5));
+            Console.WriteLine("Hello World!");
         }
     }
 }
