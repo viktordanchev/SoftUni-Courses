41a94cf7ae21b367961ba521b45d07d9fdad0ef1
VDanchev <viktordan315@gmail.com>
2023-10-26 18:20:41 +0300

    Added new solution

---
+++ CSharp/05.EntityFrameworkCore/02.EntityFrameworkIntroduction/03.EmployeesFullInformation/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+
+            var output = GetEmployeesFullInformation(context);
+            Console.WriteLine(output);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var dataString = new StringBuilder();
+
+            var employees = context.Employees
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary
+                })
+                .ToArray();
+
+            foreach (var e in employees)
+            {
+                dataString.AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}");
+            }
+
+            return dataString.ToString().Trim();
+        }
+    }
+}
