471229af5b3996d7b3d66d967b8ec9379e699a6b
VDanchev <viktordan315@gmail.com>
2023-10-26 18:20:53 +0300

    Added new solution

---
+++ CSharp/05.EntityFrameworkCore/02.EntityFrameworkIntroduction/04.EmployeesWithSalaryOver50000/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+
+            var output = GetEmployeesWithSalaryOver50000(context);
+            Console.WriteLine(output);
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var dataString = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(e => e.Salary > 50000)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .OrderBy(e => e.FirstName)
+                .ToArray();
+
+            foreach (var e in employees)
+            {
+                dataString.AppendLine($"{e.FirstName} - {e.Salary:f2}");
+            }
+
+            return dataString.ToString().Trim();
+        }
+    }
+}
