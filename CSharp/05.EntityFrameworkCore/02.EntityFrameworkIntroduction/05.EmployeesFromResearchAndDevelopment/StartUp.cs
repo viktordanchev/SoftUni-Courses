9befdef0a13328a91b73507272ee8d32c7abe145
VDanchev <viktordan315@gmail.com>
2023-10-26 18:20:55 +0300

    Added new solution

---
+++ CSharp/05.EntityFrameworkCore/02.EntityFrameworkIntroduction/05.EmployeesFromResearchAndDevelopment/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+
+            var output = GetEmployeesFromResearchAndDevelopment(context);
+            Console.WriteLine(output);
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var dataString = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(e => e.Department.Name == "Research and Development")
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.Department.Name,
+                    e.Salary
+                })
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .ToArray();
+
+            foreach (var e in employees)
+            {
+                dataString.AppendLine($"{e.FirstName} {e.LastName} from {e.Name} - ${e.Salary:f2}");
+            }
+
+            return dataString.ToString().Trim();
+        }
+    }
+}
