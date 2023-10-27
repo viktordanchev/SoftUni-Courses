797fe0c8b48eb366590ad3b5450e760f374e37e5
VDanchev <viktordan315@gmail.com>
2023-10-27 17:21:41 +0300

    Added new solution

--- CSharp/05.EntityFrameworkCore/02.EntityFrameworkIntroduction/08.AddressesByTown/StartUp.cs
+++ CSharp/05.EntityFrameworkCore/02.EntityFrameworkIntroduction/08.AddressesByTown/StartUp.cs
@@ -1,10 +1,37 @@
-﻿namespace SoftUni
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
 {
     public class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            var context = new SoftUniContext();
+            var output = GetAddressesByTown(context);
+
+            Console.WriteLine(output);
+        }
+
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+            var addresses = context.Addresses
+                .Include(x => x.Employees)
+                .Include(x => x.Town)
+                .OrderByDescending(address => address.Employees.Count)
+                .ThenBy(address => address.Town.Name)
+                .Take(10)
+                .ToList();
+
+            var result = new StringBuilder();
+
+            foreach (var a in addresses)
+            {
+                result.AppendLine($"{a.AddressText}, {a.Town.Name} - {a.Employees.Count}");
+            }
+
+            return result.ToString().Trim();
         }
     }
 }
