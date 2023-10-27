07959164762e0cea2de7fa96a5f085ea945b4f83
VDanchev <viktordan315@gmail.com>
2023-10-27 17:22:27 +0300

    Added new solution

---
+++ CSharp/05.EntityFrameworkCore/02.EntityFrameworkIntroduction/11.FindLatest10Projects/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using SoftUni.Data;
+using System.Globalization;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            var output = GetLatestProjects(context);
+
+            Console.WriteLine(output);
+        }
+
+        public static string GetLatestProjects(SoftUniContext context)
+        {
+            var projects = context.Projects
+                .OrderBy(p => p.Name)
+                .ToList();
+
+            var result = new StringBuilder();
+
+            foreach (var p in projects)
+            {
+                result.AppendLine(p.Name);
+                result.AppendLine(p.Description);
+                result.AppendLine(p.StartDate.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture));
+            }
+
+            return result.ToString().Trim();
+        }
+    }
+}
