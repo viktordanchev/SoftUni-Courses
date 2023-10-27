537caeb287000faeded4e41b4400c3521f48280d
VDanchev <viktordan315@gmail.com>
2023-10-27 17:21:56 +0300

    Update

--- CSharp/05.EntityFrameworkCore/02.EntityFrameworkIntroduction/09.Employee147/StartUp.cs
+++ CSharp/05.EntityFrameworkCore/02.EntityFrameworkIntroduction/09.Employee147/StartUp.cs
@@ -1,10 +1,43 @@
-﻿namespace SoftUni
+﻿using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
 {
     public class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            var context = new SoftUniContext();
+            var output = GetEmployee147(context);
+
+            Console.WriteLine(output);
+        }
+
+        public static string GetEmployee147(SoftUniContext context)
+        {
+            var employee = context.Employees
+                .FirstOrDefault(e => e.EmployeeId == 147);
+
+            var result = new StringBuilder();
+
+            result.AppendLine($"{employee.FirstName} {employee.LastName} - {employee.JobTitle}");
+
+            var projects = context.EmployeesProjects
+                .Select(ep => new
+                {
+                    ep.EmployeeId,
+                    ProjectName = ep.Project.Name
+                })
+                .Where(ep => ep.EmployeeId == employee.EmployeeId)
+                .OrderBy(ep => ep.ProjectName)
+                .ToList();
+
+            foreach (var p in projects)
+            {
+                result.AppendLine(p.ProjectName);
+            }
+
+            return result.ToString().Trim();
         }
     }
 }
