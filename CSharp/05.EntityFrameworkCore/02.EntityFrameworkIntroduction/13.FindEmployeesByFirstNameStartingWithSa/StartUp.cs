87ee7f5ced8b6d2e58f5390899396a43ded601ee
VDanchev <viktordan315@gmail.com>
2023-10-29 19:36:17 +0200

    Added new solution

---
+++ CSharp/05.EntityFrameworkCore/02.EntityFrameworkIntroduction/13.FindEmployeesByFirstNameStartingWithSa/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new SoftUniContext();
+            var output = GetEmployeesByFirstNameStartingWithSa(context);
+
+            Console.WriteLine(output);
+        }
+
+        public static string GetEmployeesByFirstNameStartingWithSa(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Where(e => e.FirstName.Substring(0, 2) == "Sa")
+                .OrderBy(e => e.FirstName)
+                .ThenBy(e => e.LastName)
+                .ToList();
+
+            var result = new StringBuilder();
+
+            foreach (var e in employees)
+            {
+                result.AppendLine($"{e.FirstName} {e.LastName} - {e.JobTitle} - (${e.Salary:f2})");
+            }
+
+            return result.ToString().Trim();
+        }
+    }
+}
