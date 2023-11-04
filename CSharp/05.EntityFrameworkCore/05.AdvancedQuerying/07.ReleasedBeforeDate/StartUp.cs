efeb30302d32f101f1f29a9a565840bc285c7f12
VDanchev <viktordan315@gmail.com>
2023-11-04 19:13:56 +0200

    Added new solution

---
+++ CSharp/05.EntityFrameworkCore/05.AdvancedQuerying/07.ReleasedBeforeDate/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            var output = GetBooksByPrice(db);
+
+            Console.WriteLine(output);
+        }
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var books = context.Books
+                .Select(b => new
+                {
+                    b.Title,
+                    b.Price
+                })
+                .Where(b => b.Price > 40)
+                .OrderByDescending(b => b.Price)
+                .ToList();
+
+            var result = new StringBuilder();
+
+            foreach (var b in books)
+            {
+                result.AppendLine($"{b.Title} - ${b.Price:F2}");
+            }
+
+            return result.ToString().Trim();
+        }
+    }
+}
+
+
