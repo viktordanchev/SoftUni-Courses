681c69b2e39d57bc76fb4467f703be343ed0fea3
VDanchev <viktordan315@gmail.com>
2023-11-04 18:53:36 +0200

    Added new solution

---
+++ CSharp/05.EntityFrameworkCore/05.AdvancedQuerying/03.GoldenBooks/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            var output = GetGoldenBooks(db);
+
+            Console.WriteLine(output);
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var books = context.Books
+                .Select(b => new
+                {
+                    b.BookId,
+                    b.Title,
+                    b.Copies,
+                    EditionType = b.EditionType.ToString()
+                })
+                .ToList()
+                .Where(b => b.EditionType == "Gold" && b.Copies < 5000)
+                .OrderBy(b => b.BookId);
+
+            var result = new StringBuilder();
+
+            foreach (var b in books)
+            {
+                result.AppendLine(b.Title);
+            }
+
+            return result.ToString().Trim();
+        }
+    }
+}
+
+
