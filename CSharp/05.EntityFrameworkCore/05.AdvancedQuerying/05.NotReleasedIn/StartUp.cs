b28a6c67acb50c3f417a65156abf2c31e4ba2e81
VDanchev <viktordan315@gmail.com>
2023-11-04 19:13:36 +0200

    Added new solution

---
+++ CSharp/05.EntityFrameworkCore/05.AdvancedQuerying/05.NotReleasedIn/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            var input = int.Parse(Console.ReadLine());
+            var output = GetBooksNotReleasedIn(db, input);
+
+            Console.WriteLine(output);
+        }
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+            var books = context.Books
+                .Where(b => b.ReleaseDate.Value.Year != year)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToList();
+
+            var result = new StringBuilder();
+
+            foreach (var b in books)
+            {
+                result.AppendLine(b);
+            }
+
+            return result.ToString().Trim();
+        }
+    }
+}
+
+
