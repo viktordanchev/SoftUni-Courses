f41d23ed88a73054d34c632eca06a2a2e6b55d50
VDanchev <viktordan315@gmail.com>
2023-11-06 16:29:09 +0200

    Added new project

---
+++ CSharp/05.EntityFrameworkCore/05.AdvancedQuerying/13.ProfitByCategory/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _13.ProfitByCategory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
