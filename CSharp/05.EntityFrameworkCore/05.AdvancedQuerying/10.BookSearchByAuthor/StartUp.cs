0018ba3814aebe5302f5d4f8ce7393c364c5504d
VDanchev <viktordan315@gmail.com>
2023-11-06 10:40:15 +0200

    Added new project

---
+++ CSharp/05.EntityFrameworkCore/05.AdvancedQuerying/10.BookSearchByAuthor/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+            
+            var input = Console.ReadLine();
+            var output = GetBooksReleasedBefore(db, input);
+            
+            Console.WriteLine(output);
+        }
+
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            var books = context.Books
+                .Where(b => b.ReleaseDate < DateTime.Parse(date))
+                .OrderByDescending(b => b.ReleaseDate)
+                .ToList();
+
+            var result = new StringBuilder();
+
+            foreach (var b in books)
+            {
+                result.AppendLine($"{b.Title} - {b.EditionType} - ${b.Price:F2}");
+            }
+
+            return result.ToString().Trim();
+        }
+    }
+}
+
+
