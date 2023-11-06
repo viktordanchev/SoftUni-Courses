6e22a5b3cdbe03e02e9ad3be917e85d1b97a30fb
VDanchev <viktordan315@gmail.com>
2023-11-06 16:29:13 +0200

    Added new project

---
+++ CSharp/05.EntityFrameworkCore/05.AdvancedQuerying/14.MostRecentBooks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _14.MostRecentBooks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
