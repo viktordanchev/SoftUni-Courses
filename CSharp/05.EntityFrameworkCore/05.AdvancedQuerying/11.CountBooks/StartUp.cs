8e75eedafc82a223b89d41fad670aac42dd18e8f
VDanchev <viktordan315@gmail.com>
2023-11-06 17:47:41 +0200

    Added new solution

---
+++ CSharp/05.EntityFrameworkCore/05.AdvancedQuerying/11.CountBooks/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            var input = int.Parse(Console.ReadLine());
+            var output = CountBooks(db, input);
+
+            Console.WriteLine(output);
+        }
+
+        public static int CountBooks(BookShopContext context, int lengthCheck)
+        {
+            var count = context.Books
+                .Where(b => b.Title.Length > lengthCheck)
+                .Count();
+
+            return count;
+        }
+    }
+}
+
