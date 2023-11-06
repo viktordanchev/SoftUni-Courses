8b24a65c9606928ea29b2ca46f14aa81ebabf5d3
VDanchev <viktordan315@gmail.com>
2023-11-06 10:36:49 +0200

    Update

--- CSharp/05.EntityFrameworkCore/05.AdvancedQuerying/07.ReleasedBeforeDate/StartUp.cs
+++ CSharp/05.EntityFrameworkCore/05.AdvancedQuerying/07.ReleasedBeforeDate/StartUp.cs
@@ -10,29 +10,25 @@
         {
             using var db = new BookShopContext();
             DbInitializer.ResetDatabase(db);
-
-            var output = GetBooksByPrice(db);
-
+            
+            var input = Console.ReadLine();
+            var output = GetBooksReleasedBefore(db, input);
+            
             Console.WriteLine(output);
         }
 
-        public static string GetBooksByPrice(BookShopContext context)
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
         {
             var books = context.Books
-                .Select(b => new
-                {
-                    b.Title,
-                    b.Price
-                })
-                .Where(b => b.Price > 40)
-                .OrderByDescending(b => b.Price)
+                .Where(b => b.ReleaseDate < DateTime.Parse(date))
+                .OrderByDescending(b => b.ReleaseDate)
                 .ToList();
 
             var result = new StringBuilder();
 
             foreach (var b in books)
             {
-                result.AppendLine($"{b.Title} - ${b.Price:F2}");
+                result.AppendLine($"{b.Title} - {b.EditionType} - ${b.Price:F2}");
             }
 
             return result.ToString().Trim();
