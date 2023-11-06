2f52056e4c16386982dfc9bb08fd4c963ea5ac8d
VDanchev <viktordan315@gmail.com>
2023-11-06 10:36:44 +0200

    Added new project

---
+++ CSharp/05.EntityFrameworkCore/05.AdvancedQuerying/08.AuthorSearch/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _08.AuthorSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
