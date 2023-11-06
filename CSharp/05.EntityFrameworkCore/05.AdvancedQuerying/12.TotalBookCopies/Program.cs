f537149fc3873f795fa8be6102db463bde54a9e1
VDanchev <viktordan315@gmail.com>
2023-11-06 16:29:06 +0200

    Added new project

---
+++ CSharp/05.EntityFrameworkCore/05.AdvancedQuerying/12.TotalBookCopies/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _12.TotalBookCopies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
