2dbff8fe4bab1a7ffee011d4369746ae2ea79d1a
VDanchev <viktordan315@gmail.com>
2023-11-06 19:06:34 +0200

    Added new solution

---
+++ CSharp/05.EntityFrameworkCore/05.AdvancedQuerying/14.MostRecentBooks/StartUp.cs
@@ -0,0 +1,57 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+
+            var output = GetTotalProfitByCategory(db);
+
+            Console.WriteLine(output);
+        }
+
+        public static string GetTotalProfitByCategory(BookShopContext context)
+        {
+            var categories = context.Categories
+                .OrderBy(c => c.Name)
+                .Select(c => new
+                {
+                    c.Name,
+                    Books = c.CategoryBooks
+                    .Select(b => new
+                    {
+                        Title = b.Book.Title,
+                        Year = b.Book.ReleaseDate.Value.Year
+                    })
+                    .OrderByDescending(b => b.Year)
+                    .Take(3)
+                    .ToList()
+                })
+                .ToList();
+
+            var result = new StringBuilder();
+            
+            foreach (var c in categories)
+            {
+                result.AppendLine($"--{c.Name}");
+
+                if(c.Books.Count > 0)
+                {
+                    foreach (var b in c.Books)
+                    {
+                        result.AppendLine($"{b.Title} ({b.Year})");
+                    }
+                }
+            }
+
+            return result.ToString().Trim();
+        }
+    }
+}
+
