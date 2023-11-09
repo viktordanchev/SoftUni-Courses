a49de26779cdfb109bcb326fbbe03ba8fb30d39a
VDanchev <viktordan315@gmail.com>
2023-11-09 15:33:37 +0200

    Added new project

---
+++ CSharp/06.DataStructures/02.DataStructuresAdvanced/01.B-Trees,2-3-TreesАndRed-BlackTrees/Lab/Two-ThreeTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TwoThreeTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CSharp/06.DataStructures/02.DataStructuresAdvanced/01.B-Trees,2-3-TreesАndRed-BlackTrees/Lab/Two-ThreeTree/TwoThreeTree.cs
@@ -0,0 +1,56 @@
+﻿namespace TwoThreeTree
+{
+    using System;
+    using System.Text;
+
+    public class TwoThreeTree<T> where T : IComparable<T>
+    {
+        private TreeNode<T> root;
+
+        public void Insert(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            RecursivePrint(this.root, sb);
+            return sb.ToString();
+        }
+
+        private void RecursivePrint(TreeNode<T> node, StringBuilder sb)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            if (node.LeftKey != null)
+            {
+                sb.Append(node.LeftKey).Append(" ");
+            }
+
+            if (node.RightKey != null)
+            {
+                sb.Append(node.RightKey).Append(Environment.NewLine);
+            }
+            else
+            {
+                sb.Append(Environment.NewLine);
+            }
+
+            if (node.IsTwoNode())
+            {
+                RecursivePrint(node.LeftChild, sb);
+                RecursivePrint(node.MiddleChild, sb);
+            }
+            else if (node.IsThreeNode())
+            {
+                RecursivePrint(node.LeftChild, sb);
+                RecursivePrint(node.MiddleChild, sb);
+                RecursivePrint(node.RightChild, sb);
+            }
+        }
+    }
+}
