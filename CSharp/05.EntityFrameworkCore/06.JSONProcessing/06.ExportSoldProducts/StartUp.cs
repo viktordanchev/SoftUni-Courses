4541cdbdc8db795c308025a095c3cd614266c61b
VDanchev <viktordan315@gmail.com>
2023-11-13 18:14:50 +0200

    Added new project

---
+++ CSharp/05.EntityFrameworkCore/06.JSONProcessing/06.ExportSoldProducts/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using ProductShop.Data;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new ProductShopContext();
+
+            var input = "users.json";
+            var output = ImportUsers(context, input);
+
+            Console.WriteLine(output);
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = File.ReadAllLines(inputJson);
+
+            return string.Empty;
+        }
+    }
+}
