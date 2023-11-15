dd529b9e365c8d90ebe009885c9091e5add972c1
VDanchev <viktordan315@gmail.com>
2023-11-15 20:07:39 +0200

    Added new solution

---
+++ CSharp/05.EntityFrameworkCore/06.JSONProcessing/13.ImportSales/DTOs/Import/SaleDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace CarDealer.DTOs.Import
+{
+    public class SaleDTO
+    {
+        public decimal Discount { get; set; }
+
+        public int CarId { get; set; }
+
+        public int CustomerId { get; set; }
+    }
+}
--- CSharp/05.EntityFrameworkCore/06.JSONProcessing/13.ImportSales/StartUp.cs
+++ CSharp/05.EntityFrameworkCore/06.JSONProcessing/13.ImportSales/StartUp.cs
@@ -1,10 +1,35 @@
-﻿namespace CarDealer
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
+            using var context = new CarDealerContext();
 
+            var salesJson = File.ReadAllText(@"../../../Datasets/sales.json");
+            var output = ImportSales(context, salesJson);
+
+            Console.WriteLine(output);
+        }
+
+        public static string ImportSales(CarDealerContext context, string inputJson)
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<CarDealerProfile>());
+            IMapper mapper = new Mapper(config);
+
+            var saleDTOs = JsonConvert.DeserializeObject<SaleDTO[]>(inputJson);
+            var sales = mapper.Map<Sale[]>(saleDTOs);
+
+            context.Sales.AddRange(sales);
+            context.SaveChanges();
+
+            return $"Successfully imported {sales.Length}.";
         }
     }
 }
