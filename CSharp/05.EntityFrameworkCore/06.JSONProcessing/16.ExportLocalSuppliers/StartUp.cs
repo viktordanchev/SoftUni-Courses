64895cf22b908bc006a295189786619ac98a30dd
VDanchev <viktordan315@gmail.com>
2023-11-17 17:50:20 +0200

    Added new solution

--- CSharp/05.EntityFrameworkCore/06.JSONProcessing/16.ExportLocalSuppliers/StartUp.cs
+++ CSharp/05.EntityFrameworkCore/06.JSONProcessing/16.ExportLocalSuppliers/StartUp.cs
@@ -1,10 +1,34 @@
-﻿namespace CarDealer
+﻿using CarDealer.Data;
+using Newtonsoft.Json;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
+            using var context = new CarDealerContext();
 
+            var output = GetLocalSuppliers(context);
+
+            Console.WriteLine(output);
+        }
+
+        public static string GetLocalSuppliers(CarDealerContext context)
+        {
+            var suppliers = context.Suppliers
+                .Where(s => !s.IsImporter)
+                .Select(s => new
+                {
+                    s.Id,
+                    s.Name,
+                    PartsCount = s.Parts.Count
+                })
+                .ToArray();
+
+            var suppliersJson = JsonConvert.SerializeObject(suppliers, Formatting.Indented);
+
+            return suppliersJson;
         }
     }
 }
