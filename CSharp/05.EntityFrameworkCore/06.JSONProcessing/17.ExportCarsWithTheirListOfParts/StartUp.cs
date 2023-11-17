117f7ea35f3573cb6ee84216d4939a922c10f1b3
VDanchev <viktordan315@gmail.com>
2023-11-17 18:09:37 +0200

    Added new solution

--- CSharp/05.EntityFrameworkCore/06.JSONProcessing/17.ExportCarsWithTheirListOfParts/StartUp.cs
+++ CSharp/05.EntityFrameworkCore/06.JSONProcessing/17.ExportCarsWithTheirListOfParts/StartUp.cs
@@ -1,10 +1,50 @@
-﻿namespace CarDealer
+﻿using CarDealer.Data;
+using Newtonsoft.Json;
+using System.Xml.Linq;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
+            using var context = new CarDealerContext();
 
+            var output = GetCarsWithTheirListOfParts(context);
+
+            Console.WriteLine(output);
+        }
+
+        public static string GetCarsWithTheirListOfParts(CarDealerContext context)
+        {
+            var cars = context.Cars
+                .Select(c => new
+                {
+                    c.Make,
+                    c.Model,
+                    c.TraveledDistance,
+                    Parts = c.PartsCars
+                    .Select(p => new
+                    {
+                        p.Part.Name,
+                        Price = p.Part.Price.ToString("F2")
+                    })
+                    .ToList()
+                })
+                .ToArray();
+
+            var carsJson = JsonConvert.SerializeObject(cars.Select(c => new
+            {
+                car = new
+                {
+                    c.Make,
+                    c.Model,
+                    c.TraveledDistance
+                },
+                parts = c.Parts
+            }), Formatting.Indented);
+
+            return carsJson;
         }
     }
 }
