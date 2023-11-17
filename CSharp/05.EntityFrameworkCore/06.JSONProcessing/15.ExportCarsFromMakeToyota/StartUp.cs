df704134a75be363b12be70ac5af1ed64dc0228c
VDanchev <viktordan315@gmail.com>
2023-11-17 17:24:18 +0200

    Update

--- CSharp/05.EntityFrameworkCore/06.JSONProcessing/15.ExportCarsFromMakeToyota/StartUp.cs
+++ CSharp/05.EntityFrameworkCore/06.JSONProcessing/15.ExportCarsFromMakeToyota/StartUp.cs
@@ -1,10 +1,36 @@
-﻿namespace CarDealer
+﻿using CarDealer.Data;
+using Newtonsoft.Json;
+using System.Globalization;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
+            using var context = new CarDealerContext();
 
+            var output = GetOrderedCustomers(context);
+
+            Console.WriteLine(output);
+        }
+
+        public static string GetOrderedCustomers(CarDealerContext context)
+        {
+            var customers = context.Customers
+                .OrderBy(c => c.BirthDate)
+                .ThenBy(c => c.IsYoungDriver)
+                .Select(c => new
+                {
+                    c.Name,
+                    BirthDate = c.BirthDate.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    c.IsYoungDriver
+                })
+            .ToArray();
+
+            var customersJson = JsonConvert.SerializeObject(customers, Formatting.Indented);
+
+            return customersJson;
         }
     }
 }
