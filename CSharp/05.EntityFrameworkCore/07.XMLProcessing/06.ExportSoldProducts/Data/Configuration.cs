fe6e1688051b7095be558d06d91df5eabe663038
VDanchev <viktordan315@gmail.com>
2023-11-19 18:12:32 +0200

    Added new project

---
+++ CSharp/05.EntityFrameworkCore/07.XMLProcessing/06.ExportSoldProducts/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace ProductShop.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = @"Server=VIKTOR-PC\SQLEXPRESS;Database=ProductShop;Trusted_Connection=True";
+    }
+}
