bcf1b59ce4a9621363fda745ca3b89be271094a4
VDanchev <viktordan315@gmail.com>
2023-11-22 16:34:17 +0200

    Added new project

---
+++ CSharp/06.DataStructures/02.DataStructuresAdvanced/01.B-Trees,2-3-TreesАndRed-BlackTrees/Exercise/RedBlackTree/IBinarySearchTree.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.RedBlackTree
+{
+    using System;
+    using System.Collections.Generic;
+
+    public interface IBinarySearchTree<T> where T: IComparable
+    {
+        void Insert(T element);
+        bool Contains(T element);
+        void EachInOrder(Action<T> action);
+
+        IBinarySearchTree<T> Search(T element);
+        void Delete(T element);
+        void DeleteMin();
+        int Count();
+        int Rank(T element);
+        T Select(int rank);
+        T Ceiling(T element);
+        T Floor(T element);
+        IEnumerable<T> Range(T startRange, T endRange);
+    }
+}
---
+++ CSharp/06.DataStructures/02.DataStructuresAdvanced/01.B-Trees,2-3-TreesАndRed-BlackTrees/Exercise/RedBlackTree/RedBlackTree.cs
@@ -0,0 +1,61 @@
+﻿namespace _01.RedBlackTree
+{
+    using System;
+
+    public class RedBlackTree<T> where T : IComparable
+    {
+        public class Node
+        {
+            public Node(T value)
+            {
+                this.Value = value;
+            }
+
+            public T Value { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+        }
+
+        public Node root;
+
+        public RedBlackTree()
+        {
+
+        }
+
+        public void EachInOrder(Action<T> action)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RedBlackTree<T> Search(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T value)
+        {
+            throw new NotImplementedException();
+        } 
+
+        public void Delete(T key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteMin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteMax()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
