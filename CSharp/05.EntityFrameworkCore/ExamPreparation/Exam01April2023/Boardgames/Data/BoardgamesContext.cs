3ed97cce1feef5f7aab37cc73a153f5f1eb4cd82
VDanchev <viktordan315@gmail.com>
2023-11-27 16:33:51 +0200

    New porject added

---
+++ CSharp/05.EntityFrameworkCore/ExamPreparation/Exam01April2023/Boardgames/Data/BoardgamesContext.cs
@@ -0,0 +1,29 @@
+﻿namespace Boardgames.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    
+    public class BoardgamesContext : DbContext
+    {
+        public BoardgamesContext()
+        { 
+        }
+
+        public BoardgamesContext(DbContextOptions options)
+            : base(options) 
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
