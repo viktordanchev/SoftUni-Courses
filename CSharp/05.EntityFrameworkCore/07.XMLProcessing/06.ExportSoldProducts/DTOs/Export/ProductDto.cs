1ec32de7994913f3db5cb7b2c0e0bf81c67db892
VDanchev <viktordan315@gmail.com>
2023-11-29 17:20:08 +0200

    Deleted projects

---
+++ CSharp/05.EntityFrameworkCore/07.XMLProcessing/06.ExportSoldProducts/DTOs/Export/ProductDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace _06.ExportSoldProducts.DTOs.Export
+{
+    [XmlType("Product")]
+    public class ProductDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; } = null!;
+
+        [XmlElement("price")]
+        public decimal Price { get; set; }
+    }
+}
--- CSharp/05.EntityFrameworkCore/07.XMLProcessing/06.ExportSoldProducts/StartUp.cs
+++ CSharp/05.EntityFrameworkCore/07.XMLProcessing/06.ExportSoldProducts/StartUp.cs
@@ -1,10 +1,50 @@
-﻿namespace ProductShop
+﻿using _06.ExportSoldProducts.DTOs.Export;
+using ProductShop.Data;
+using System.Xml.Serialization;
+
+namespace ProductShop
 {
     public class StartUp
     {
         public static void Main()
         {
+            using var context = new ProductShopContext();
 
+            var output = GetSoldProducts(context);
+
+            Console.WriteLine(output);
+        }
+
+        public static string GetSoldProducts(ProductShopContext context)
+        {
+            var users = context.Users
+                .Where(u => u.ProductsSold.Count > 0)
+                .Select(u => new UserDto()
+                {
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    SoldProducts = u.ProductsSold
+                        .Select(p => new ProductDto()
+                        { 
+                            Name = p.Name,
+                            Price = p.Price,
+                        })
+                        .ToArray()
+                })
+                .OrderBy(u => u.LastName)
+                .ThenBy(u => u.FirstName)
+                .Take(5)
+                .ToArray();
+
+            var serializer = new XmlSerializer(typeof(UserDto[]), new XmlRootAttribute("Users"));
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+            ns.Add(string.Empty, string.Empty);
+
+            using (var usersXml = new StringWriter())
+            {
+                serializer.Serialize(usersXml, users, ns);
+                return usersXml.ToString();
+            }
         }
     }
 }
