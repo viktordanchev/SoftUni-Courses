13898c96fbdce90e4cb84ec973604bd628510472
VDanchev <viktordan315@gmail.com>
2023-11-29 15:49:21 +0200

    Added new project

---
+++ CSharp/05.EntityFrameworkCore/ExamPreparation/RetakeExam15August2022/Trucks/Data/TrucksContext.cs
@@ -0,0 +1,30 @@
+﻿namespace Trucks.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class TrucksContext : DbContext
+    {
+        public TrucksContext()
+        { 
+        }
+
+        public TrucksContext(DbContextOptions options)
+            : base(options) 
+        { 
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
