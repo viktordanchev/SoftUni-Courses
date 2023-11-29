9476ccbba6341e910d680b32bfa22b565444a36a
VDanchev <viktordan315@gmail.com>
2023-11-29 17:31:49 +0200

    Added new projects

---
+++ CSharp/05.EntityFrameworkCore/07.XMLProcessing/07.ExportCategoriesByProductsCount/DTOs/Import/CategoryProductDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace ProductShop.DTOs.Import
+{
+    [XmlType("CategoryProduct")]
+    public class CategoryProductDto
+    {
+        [XmlElement("CategoryId")]
+        public int CategoryId { get; set; }
+
+        [XmlElement("ProductId")]
+        public int ProductId { get; set; }
+    }
+}
