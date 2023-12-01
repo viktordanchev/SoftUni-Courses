1d1fed4f508c632f7db23468df56015a8a4486d0
VDanchev <viktordan315@gmail.com>
2023-12-01 11:29:18 +0200

    Added new project

---
+++ CSharp/05.EntityFrameworkCore/ExamPreparation/Exam06August2022/Footballers/Data/FootballersContext.cs
@@ -0,0 +1,26 @@
+﻿namespace Footballers.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class FootballersContext : DbContext
+    {
+        public FootballersContext() { }
+
+        public FootballersContext(DbContextOptions options)
+            : base(options) { }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
