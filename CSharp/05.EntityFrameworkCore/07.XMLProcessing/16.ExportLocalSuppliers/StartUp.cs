28fcea9240b09f95e9701ab744cd8f8a9ab2d2c3
VDanchev <viktordan315@gmail.com>
2023-12-05 20:19:31 +0200

    Added new solution

---
+++ CSharp/05.EntityFrameworkCore/07.XMLProcessing/16.ExportLocalSuppliers/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Export;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new CarDealerContext();
+
+            var output = GetLocalSuppliers(context);
+
+            Console.WriteLine(output);
+        }
+
+        public static string GetLocalSuppliers(CarDealerContext context)
+        {
+            var cars = context.Suppliers
+                .Where(s => !s.IsImporter)
+                .Select(c => new SupplierDto()
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    PartsCount = c.Parts.Count
+                })
+                .ToArray();
+
+            var serializer = new XmlSerializer(typeof(SupplierDto[]), new XmlRootAttribute("suppliers"));
+            var ns = new XmlSerializerNamespaces();
+            ns.Add(string.Empty, string.Empty);
+
+            using (var supplierDtoXml = new StringWriter())
+            {
+                serializer.Serialize(supplierDtoXml, cars, ns);
+                return supplierDtoXml.ToString();
+            }
+        }
+    }
+}
