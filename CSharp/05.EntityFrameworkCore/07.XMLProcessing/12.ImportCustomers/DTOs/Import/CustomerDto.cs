f3a78f671139d425ac910e78e141e322348deef3
VDanchev <viktordan315@gmail.com>
2023-12-05 19:45:45 +0200

    Added new solution

---
+++ CSharp/05.EntityFrameworkCore/07.XMLProcessing/12.ImportCustomers/DTOs/Import/CustomerDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Import
+{
+    [XmlType("Customer")]
+    public class CustomerDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; } = null!;
+
+        [XmlElement("birthDate")]
+        public string BirthDate { get; set; } = null!;
+
+        [XmlElement("isYoungDriver")]
+        public bool IsYoungDriver { get; set; }
+    }
+}
---
+++ CSharp/05.EntityFrameworkCore/07.XMLProcessing/12.ImportCustomers/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using CarDealer.DTOs.Import;
+using System.Xml.Serialization;
+using System.Linq;
+using System.Globalization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new CarDealerContext();
+
+            var customersXml = File.ReadAllText(@"../../../Datasets/customers.xml");
+            var output = ImportCustomers(context, customersXml);
+
+            Console.WriteLine(output);
+        }
+
+        public static string ImportCustomers(CarDealerContext context, string inputXml)
+        {
+            var serializer = new XmlSerializer(typeof(CustomerDto[]), new XmlRootAttribute("Customers"));
+            var customerDtos = (CustomerDto[])serializer.Deserialize(new StringReader(inputXml));
+
+            var customers = new List<Customer>();
+
+            foreach (var customerDto in customerDtos)
+            {
+                DateTime parsedBirthDate;
+                var parseBirthDate = DateTime.TryParseExact(customerDto.BirthDate, "yyyy-MM-dd'T'HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedBirthDate);
+
+                var customer = new Customer()
+                {
+                    Name = customerDto.Name,
+                    BirthDate = parsedBirthDate,
+                    IsYoungDriver = customerDto.IsYoungDriver
+                };
+
+                customers.Add(customer);
+            }
+
+            context.Customers.AddRange(customers);
+            context.SaveChanges();
+
+            return $"Successfully imported {customers.Count}";
+        }
+    }
+}
