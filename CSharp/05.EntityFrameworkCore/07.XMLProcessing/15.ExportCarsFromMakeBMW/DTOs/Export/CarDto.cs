2d96f743a8877a24213ca2f50dd184f7a5bb83bd
VDanchev <viktordan315@gmail.com>
2023-12-05 20:13:42 +0200

    Added new solution

---
+++ CSharp/05.EntityFrameworkCore/07.XMLProcessing/15.ExportCarsFromMakeBMW/DTOs/Export/CarDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Export
+{
+    [XmlType("car")]
+    public class CarDto
+    {
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+
+        [XmlAttribute("model")]
+        public string Model { get; set; } = null!;
+
+        [XmlAttribute("traveled-distance")]
+        public long TraveledDistance { get; set; }
+    }
+}
---
+++ CSharp/05.EntityFrameworkCore/07.XMLProcessing/15.ExportCarsFromMakeBMW/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Export;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new CarDealerContext();
+
+            var output = GetCarsFromMakeBmw(context);
+
+            Console.WriteLine(output);
+        }
+
+        public static string GetCarsFromMakeBmw(CarDealerContext context)
+        {
+            var cars = context.Cars
+                .Where(c => c.Make == "BMW")
+                .Select(c => new CarDto()
+                {
+                    Id = c.Id,
+                    Model = c.Model,
+                    TraveledDistance = c.TraveledDistance
+                })
+                .OrderBy(c => c.Model)
+                .ThenByDescending(c => c.TraveledDistance)
+                .ToArray();
+
+            var serializer = new XmlSerializer(typeof(CarDto[]), new XmlRootAttribute("cars"));
+            var ns = new XmlSerializerNamespaces();
+            ns.Add(string.Empty, string.Empty);
+
+            using (var carXml = new StringWriter())
+            {
+                serializer.Serialize(carXml, cars, ns);
+                return carXml.ToString();
+            }
+        }
+    }
+}
