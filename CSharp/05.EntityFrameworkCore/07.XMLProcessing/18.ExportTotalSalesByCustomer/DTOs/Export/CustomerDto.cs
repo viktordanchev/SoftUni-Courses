733dc031f73a84ebe2c934101c3a3ee669c1723d
VDanchev <viktordan315@gmail.com>
2023-12-05 21:15:47 +0200

    Added new project

---
+++ CSharp/05.EntityFrameworkCore/07.XMLProcessing/18.ExportTotalSalesByCustomer/DTOs/Export/CustomerDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Export
+{
+    [XmlType("customer")]
+    public class CustomerDto
+    {
+        [XmlAttribute("full-name")]
+        public string Name { get; set; } = null!;
+
+        [XmlAttribute("bought-cars")]
+        public int BoughtCars { get; set; }
+
+        [XmlAttribute("spent-money")]
+        public decimal SpentMoney { get; set; }
+    }
+}
---
+++ CSharp/05.EntityFrameworkCore/07.XMLProcessing/18.ExportTotalSalesByCustomer/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Export;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var context = new CarDealerContext();
+
+            var output = GetTotalSalesByCustomer(context);
+
+            Console.WriteLine(output);
+        }
+
+        public static string GetTotalSalesByCustomer(CarDealerContext context)
+        {
+            var cars = context.Customers
+                .Where(c => c.)
+                .Select(c => new CustomerDto()
+                {
+                    Make = c.Make,
+                    Model = c.Model,
+                    TraveledDistance = c.TraveledDistance,
+                    Parts = c.PartsCars
+                        .Select(p => new PartDto()
+                        {
+                            Name = p.Part.Name,
+                            Price = p.Part.Price
+                        })
+                        .OrderByDescending(p => p.Price)
+                        .ToArray()
+                })
+                .OrderByDescending(c => c.TraveledDistance)
+                .ThenBy(c => c.Model)
+                .Take(5)
+                .ToArray();
+
+            var serializer = new XmlSerializer(typeof(CarDto[]), new XmlRootAttribute("cars"));
+            var ns = new XmlSerializerNamespaces();
+            ns.Add(string.Empty, string.Empty);
+
+            using (var carXml = new StringWriter())
+            {
+                serializer.Serialize(carXml, cars, ns);
+                return carXml.ToString();
+            }
+        }
+    }
+}
