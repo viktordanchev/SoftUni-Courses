607518309be014b4de117ea0063b6567c3173264
VDanchev <viktordan315@gmail.com>
2023-12-16 17:03:24 +0200

    Added new project

---
+++ CSharp/06.DataStructures/03.HashTablesSetsAndDictionaries/Lab/HashTable/HashTable.cs
@@ -0,0 +1,99 @@
+﻿namespace HashTable
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    
+    public class HashTable<TKey, TValue> : IEnumerable<KeyValue<TKey, TValue>>
+    {
+        public int Count { get; private set; }
+
+        public int Capacity => throw new NotImplementedException();
+
+        public HashTable() { throw new NotImplementedException(); }
+
+        public HashTable(int capacity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddOrReplace(TKey key, TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TValue Get(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            var element = this.Find(key);
+
+            if(element != null)
+            {
+                value = element.Value;
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
+
+        public KeyValue<TKey, TValue> Find(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TKey> Keys => throw new NotImplementedException();
+
+        public IEnumerable<TValue> Values
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IEnumerator<KeyValue<TKey, TValue>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => throw new NotImplementedException();
+    }
+}
