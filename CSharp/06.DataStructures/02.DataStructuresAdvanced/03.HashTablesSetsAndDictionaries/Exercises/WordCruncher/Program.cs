9cba13651a99859040026fbe32a799d2edbed77e
VDanchev <viktordan315@gmail.com>
2023-12-16 17:14:58 +0200

    Added new project

---
+++ CSharp/06.DataStructures/02.DataStructuresAdvanced/03.HashTablesSetsAndDictionaries/Exercises/WordCruncher/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WordCruncher
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
