69cf55c2d2325764a73711567329165ea93f48f9
VDanchev <viktordan315@gmail.com>
2024-01-04 19:00:08 +0200

    Added new project

---
+++ CSharp/07.Algorithms/01.Fundamentals/01.RecursionAndBacktrackingLab/06.8QueensPuzzle/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _06._8QueensPuzzle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
