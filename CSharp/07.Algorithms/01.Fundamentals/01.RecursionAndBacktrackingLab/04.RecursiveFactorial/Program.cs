2a7500bfdb098c2f41069086dec53d9ab39412da
VDanchev <viktordan315@gmail.com>
2024-01-08 19:11:59 +0200

    Added new solution

--- CSharp/07.Algorithms/01.Fundamentals/01.RecursionAndBacktrackingLab/04.RecursiveFactorial/Program.cs
+++ CSharp/07.Algorithms/01.Fundamentals/01.RecursionAndBacktrackingLab/04.RecursiveFactorial/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int nubmer = int.Parse(Console.ReadLine());
+            int factorial = GetFactorial(nubmer);
+
+            Console.WriteLine(factorial);
+        }
+
+        static public int GetFactorial(int nubmer)
+        {
+            if(nubmer == 0)
+            {
+                return 1;
+            }
+
+            return nubmer * GetFactorial(nubmer - 1);
         }
     }
 }
