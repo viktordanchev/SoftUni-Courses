331af243327d360c2d0b433f6814b7c638349e94
VDanchev <viktordan315@gmail.com>
2024-01-09 16:25:51 +0200

    Added new solution

--- CSharp/07.Algorithms/01.Fundamentals/01.RecursionAndBacktrackingLab/01.RecursiveArraySum/Program.cs
+++ CSharp/07.Algorithms/01.Fundamentals/01.RecursionAndBacktrackingLab/01.RecursiveArraySum/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace _01.RecursiveArraySum
+﻿
+namespace _01.RecursiveArraySum
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = Console.ReadLine().Split(" ").Select(n => int.Parse(n)).ToArray();
+            int numbersSum = Sum(numbers, 0);
+
+            Console.WriteLine(numbersSum);
+        }
+
+        private static int Sum(int[] numbers, int index)
+        {
+            if(index + 1 == numbers.Length)
+            {
+                return numbers[index];
+            }
+
+            return numbers[index] + Sum(numbers, index + 1);
         }
     }
 }
