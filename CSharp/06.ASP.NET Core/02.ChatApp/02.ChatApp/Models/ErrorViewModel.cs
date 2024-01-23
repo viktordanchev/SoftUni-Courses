c015aa314ea0bc7cbaa7f2dcb7b80f8b8afd3804
VDanchev <viktordan315@gmail.com>
2024-01-23 13:57:42 +0200

    Added new solution

---
+++ CSharp/06.ASP.NET Core/02.ChatApp/02.ChatApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _02.ChatApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
