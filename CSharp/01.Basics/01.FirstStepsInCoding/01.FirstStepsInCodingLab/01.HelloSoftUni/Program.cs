ea6df3e7c213561dfc947843a4aa8b9a96febba5
VDanchev <viktordan315@gmail.com>
2024-01-29 10:21:03 +0200

    Updated

--- CSharp/01.Basics/01.FirstStepsInCoding/01.FirstStepsInCodingLab/01.HelloSoftUni/Program.cs
+++ CSharp/01.Basics/01.FirstStepsInCoding/01.FirstStepsInCodingLab/01.HelloSoftUni/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace _01.HelloSoftUni
 {
@@ -7,34 +6,7 @@ namespace _01.HelloSoftUni
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(IsValid("([]])"));
-        }
-
-        public static bool IsValid(string s)
-        {
-            var parentheses = s.ToCharArray();
-            var stack = new Stack<char>();
-
-            for (int i = 0; i < parentheses.Length; i++)
-            {
-                var curr = parentheses[i];
-
-                stack.Push(curr);
-
-                if (curr == ')' || curr == '}' || curr == ']')
-                {
-                    stack.Pop();
-
-                    if(stack.Count == 0)
-                    {
-                        break;
-                    }
-
-                    stack.Pop();
-                }
-            }
-
-            return stack.Count == 0;
+            Console.WriteLine("Hello SoftUni");
         }
     }
 }
