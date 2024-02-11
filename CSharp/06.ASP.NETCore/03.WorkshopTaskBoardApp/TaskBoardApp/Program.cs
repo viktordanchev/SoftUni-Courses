1e8bf8825a01f32276d82eff8c18d7660a7e6a4c
VDanchev <viktordan315@gmail.com>
2024-02-11 17:00:56 +0200

    Updated

--- CSharp/06.ASP.NETCore/03.WorkshopTaskBoardApp/TaskBoardApp/Program.cs
+++ CSharp/06.ASP.NETCore/03.WorkshopTaskBoardApp/TaskBoardApp/Program.cs
@@ -10,7 +10,14 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+})
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
