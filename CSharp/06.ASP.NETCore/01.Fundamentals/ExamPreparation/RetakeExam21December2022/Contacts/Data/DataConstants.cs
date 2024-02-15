fc2a59f6ceace2013bc37a37bc74eacf16fe9d3c
VDanchev <viktordan315@gmail.com>
2024-02-15 19:49:33 +0200

    Added new solution

---
+++ CSharp/06.ASP.NETCore/01.Fundamentals/ExamPreparation/RetakeExam21December2022/Contacts/Data/DataConstants.cs
@@ -0,0 +1,29 @@
+﻿namespace Contacts.Data
+{
+    public static class DataConstants
+    {
+        public const int EmailMinLength = 10;
+        public const int EmailMaxLength = 60;
+
+        public static class ApplicationUser
+        {
+            public const int UserNameMinLength = 5;
+            public const int UserNameMaxLength = 20;
+
+            public const int PasswordMinLength = 5;
+            public const int PasswordMaxLength = 20;
+        }
+
+        public static class Contact
+        {
+            public const int FirstNameMinLength = 2;
+            public const int FirstNameMaxLength = 50;
+
+            public const int LastNameMinLength = 5;
+            public const int LastNameMaxLength = 50;
+
+            public const int PhoneNumberMinLength = 10;
+            public const int PhoneNumberMaxLength = 13;
+        }
+    }
+}
---
+++ CSharp/06.ASP.NETCore/01.Fundamentals/ExamPreparation/RetakeExam21December2022/Contacts/Data/Models/ApplicationUser.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Contacts.Data.Models
+{
+    public class ApplicationUser
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(DataConstants.ApplicationUser.UserNameMinLength)]
+        [MaxLength(DataConstants.ApplicationUser.UserNameMaxLength)]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        [MinLength(DataConstants.ApplicationUser.EmailMinLength)]
+        [MaxLength(DataConstants.ApplicationUser.EmailMaxLength)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [MinLength(DataConstants.ApplicationUser.PasswordMinLength)]
+        [MaxLength(DataConstants.ApplicationUser.PasswordMaxLength)]
+        public string Password { get; set; } = null!;
+    }
+}
