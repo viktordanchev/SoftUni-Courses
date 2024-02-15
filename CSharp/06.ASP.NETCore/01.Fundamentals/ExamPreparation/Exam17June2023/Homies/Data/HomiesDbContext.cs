b12b0c313f828e373d1bf27a68f449b362a08c18
VDanchev <viktordan315@gmail.com>
2024-02-15 14:12:27 +0200

    Added new solution

---
+++ CSharp/06.ASP.NETCore/01.Fundamentals/ExamPreparation/Exam17June2023/Homies/Data/HomiesDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homies.Data
+{
+    public class HomiesDbContext : IdentityDbContext
+    {
+        public HomiesDbContext(DbContextOptions<HomiesDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder
+            //    .Entity<Type>()
+            //    .HasData(new Type()
+            //    {
+            //        Id = 1,
+            //        Name = "Animals"
+            //    },
+            //    new Type()
+            //    {
+            //        Id = 2,
+            //        Name = "Fun"
+            //    },
+            //    new Type()
+            //    {
+            //        Id = 3,
+            //        Name = "Discussion"
+            //    },
+            //    new Type()
+            //    {
+            //        Id = 4,
+            //        Name = "Work"
+            //    });
+
+            //base.OnModelCreating(modelBuilder);
+        }
+    }
+}
