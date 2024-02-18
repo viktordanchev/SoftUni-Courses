4202fed8f6777d40d5e952abf5631056690ef3fc
VDanchev <viktordan315@gmail.com>
2024-02-18 14:02:27 +0200

    Added new solution

--- CSharp/01.Basics/01.FirstStepsInCoding/01.FirstStepsInCodingLab/01.HelloSoftUni/Program.cs
+++ CSharp/01.Basics/01.FirstStepsInCoding/01.FirstStepsInCodingLab/01.HelloSoftUni/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01.HelloSoftUni
 {
@@ -6,7 +8,32 @@ namespace _01.HelloSoftUni
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello SoftUni");
+            Console.WriteLine(ThreeSum(new int[] { 0, 3, 0, 1, 1, -1, -5, -5, 3, -3, -3, 0 }));
+        }
+
+        public static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+
+            for (int f = 0; f < nums.Length; f++)
+            {
+                for (int s = f + 1; s < nums.Length; s++)
+                {
+                    for (int t = s + 1; t < nums.Length; t++)
+                    {
+                        var currList = new List<int> { nums[f], nums[s], nums[t] };
+                        currList.Sort();
+
+                        if (nums[f] + nums[s] + nums[t] == 0 &&
+                            result.All(l => !l.SequenceEqual(currList)))
+                        {
+                            result.Add(currList);
+                        }
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
---
+++ CSharp/06.ASP.NETCore/01.Fundamentals/FinalExam/SeminarHub/Data/SeminarHubDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SeminarHub.Data
+{
+    public class SeminarHubDbContext : IdentityDbContext
+    {
+        public SeminarHubDbContext(DbContextOptions<SeminarHubDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //builder
+            //   .Entity<Category>()
+            //   .HasData(new Category()
+            //   {
+            //       Id = 1,
+            //       Name = "Technology & Innovation"
+            //   },
+            //   new Category()
+            //   {
+            //       Id = 2,
+            //       Name = "Business & Entrepreneurship"
+            //   },
+            //   new Category()
+            //   {
+            //       Id = 3,
+            //       Name = "Science & Research"
+            //   },
+            //   new Category()
+            //   {
+            //       Id = 4,
+            //       Name = "Arts & Culture"
+            //   });
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
