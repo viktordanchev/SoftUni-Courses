7e9a852f37edeb12822f2bb0a382750869296db7
VDanchev <viktordan315@gmail.com>
2024-04-17 09:58:26 +0300

    Added new solution

---
+++ CSharp/06.ASP.NETCore/FinalExam/GameZone/Data/GameZoneDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameZone.Data
+{
+    public class GameZoneDbContext : IdentityDbContext
+    {
+        public GameZoneDbContext(DbContextOptions<GameZoneDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //base.OnModelCreating(builder);
+
+            //builder
+            //    .Entity<Genre>()
+            //    .HasData(
+            //    new Genre { Id = 1, Name = "Action" },
+            //    new Genre { Id = 2, Name = "Adventure" },
+            //    new Genre { Id = 3, Name = "Fighting" },
+            //    new Genre { Id = 4, Name = "Sports" },
+            //    new Genre { Id = 5, Name = "Racing" },
+            //    new Genre { Id = 6, Name = "Strategy" });
+        }
+    }
+}
